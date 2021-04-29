24721d0636078f9e2916e19634762d5a2025027e
tolmany <jollydev0821@gmail.com>
2021-04-29 16:24:34 +0200

    Global Styled initialised

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+// Imports for styling
+import GlobalStyle from "./themes/GlobalStyle";
+
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <p>Wallfair.</p>
     </div>
   );
