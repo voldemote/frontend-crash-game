08c755595c1de762c7265cffdbbf36eb82e1e5b4
tolmany <jollydev0821@gmail.com>
2021-05-17 19:03:30 +0200

    header slider mobile and desktop

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,45 @@
 import LiveBadge from '../LiveBadge';
-import React     from 'react';
+import React, { useRef } from 'react';
 import styles    from './styles.module.scss';
 import Tags      from '../Tags';
+import Link      from '../../components/Link';
+import Routes    from '../../constants/Routes';
+import useSlider from '../../helper/useSlider';
 
-const Header = ({ backgroundImage, title, tags }) => {
-    const getContainerStyle = () => {
-        return {
-            backgroundImage: 'url("' + backgroundImage + '")',
-        };
-    };
+const Header = ({ slides }) => {
+
+    const slideImage = useRef(null)
+    const slideText = useRef(null)
+    const slideTags = useRef(null)
+    const { goToPreviousSlide, goToNextSlide } = useSlider(slideImage, slideText, slideTags, slides)
 
     return (
-        <div
-            className={styles.header}
-            style={getContainerStyle()}
-        >
-            <div className={styles.headerOverlay}>
-            </div>
-            <div className={styles.headerContentContainer}>
-                <div className={styles.badgeContainer}>
-                    <LiveBadge />
+        <div className={styles.header} ref={slideImage}>
+            <div className={styles.headerOverlay}></div>
+            <div className={styles.headerWrapper}>
+                <div className={styles.headerContentContainer}>
+                    <div className={styles.badgeContainer}>
+                        <LiveBadge />
+                    </div>
+                    <span ref={slideText} className={styles.title}></span>
+                    <div ref={slideTags} className={styles.tags}></div>
+                    <div>
+                        <Link to={true} className={styles.goToEvent}>
+                            <span>Go to event</span>
+                            <div className={styles.arrowRight}></div>
+                        </Link>
+                    </div>
                 </div>
-                <span className={styles.title}>
-                    {title}
-                </span>
-                <Tags tags={tags}/>
+                <div className={styles.switchButtons}>
+                    <button onClick={goToPreviousSlide} className={styles.goToPreviousSlide}>
+                        <span></span>
+                    </button>
+                    <button onClick={goToNextSlide} className={styles.goToNextSlide}>
+                        <span></span>
+                    </button>
+                </div>
             </div>
+
         </div>
     );
 };
