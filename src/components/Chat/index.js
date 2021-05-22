7be25b451c6c1a59a6b894720af34adb3b508530
tolmany <jollydev0821@gmail.com>
2021-05-22 15:34:03 +0200

    mobile screen part 1

---
+++ src/components/Chat/index.js
@@ -0,0 +1,25 @@
+import styles              from './styles.module.scss';
+import ExampleProfilePicture from '../../data/images/doge.jpg';
+import ChatBubble from 'components/chatBubble';
+
+const Chat = () => {
+
+    return (
+        <div>
+            <div className={styles.chat}>
+                <ChatBubble name={"test"} image={ExampleProfilePicture} message={"Lorem ipsum Lorem ipsum Lorem ipsum "} date={"just now"} />
+                <ChatBubble name={"test"} image={ExampleProfilePicture} message={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem"} date={"just now"} />
+                <ChatBubble name={"test"} image={ExampleProfilePicture} message={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "} date={"just now"} />
+                <ChatBubble name={"test"} image={ExampleProfilePicture} message={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "} date={"just now"} />
+                <ChatBubble name={"test"} image={ExampleProfilePicture} message={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ipsum "} date={"just now"} />
+                <div className={styles.overlay}></div>
+            </div>
+            <form className={styles.messageInput}>
+                <input id="message" type="text" name="message" placeholder="Comment..." />
+                <button type="submit" onClick={() => console.log("sent")}>Send</button>
+            </form>
+        </div>
+    );
+};
+
+export default Chat;
