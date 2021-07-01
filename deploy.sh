e3a230d965b5ee74225f2fe94b5d5b8f7af1e76e
tolmany <jollydev0821@gmail.com>
2021-07-01 23:35:10 +0200

    changes for new staging

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+defaultversion=dev
+VERSION=${1:-$defaultversion}
+
 npm run build
 doctl registry login
-docker build . -t registry.digitalocean.com/wallfair/frontend
-docker push registry.digitalocean.com/wallfair/frontend
+docker build . -t registry.digitalocean.com/wallfair/frontend:$VERSION
+docker push registry.digitalocean.com/wallfair/frontend:$VERSION
