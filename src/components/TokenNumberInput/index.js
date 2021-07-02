470bff44d9adbb38897258365f75f0ee75230ff4
tolmany <jollydev0821@gmail.com>
2021-07-02 15:22:38 +0200

    chore: ML-35 validate number input  and prevent edge cases

--- src/components/TokenNumberInput/index.js
+++ src/components/TokenNumberInput/index.js
@@ -1,44 +1,37 @@
-import React         from 'react';
-import styles        from './styles.module.scss';
-import Input         from '../Input';
-import classNames    from 'classnames';
-import _             from 'lodash';
-import ErrorHint     from '../ErrorHint';
-import { useState }  from 'react';
-import { useEffect } from 'react';
+import React from 'react';
+import styles from './styles.module.scss';
+import Input from '../Input';
+import classNames from 'classnames';
+import _ from 'lodash';
+import ErrorHint from '../ErrorHint';
 
 const TokenNumberInput = ({ value, setValue, maxValue, errorText, className, ...props }) => {
-    const onChange = (event) => {
-        const value       = _.get(event, 'target.value', 0);
-        const targetValue = _.toNumber(value);
+  const onChange = (event) => {
+    const value = _.get(event, 'target.value', 0);
+    // @TODO: this needs refactoring imo, a validation function, a base form component that this could based upon or render and may be even refactor the parent forms to a lib like react-final-form or similar
+    // remove leading zero(s) and cast to number
+    const regex = new RegExp('^0+(?!$)', 'g');
+    let targetValue = _.toNumber(value.replaceAll(regex, ''));
 
-        setValue(targetValue);
-    };
-    return (
-        <>
-            <div
-                className={classNames(
-                    styles.tokenNumberInputContainer,
-                    className,
-                )}
-            >
-                <Input
-                    className={styles.input}
-                    type={'number'}
-                    value={value}
-                    onChange={onChange}
-                    {...props}
-                />
-                <span className={styles.eventTokenLabel}>
-                    EVNT
-                </span>
-            </div>
-            <ErrorHint
-                className={styles.tokenNumberErrorHint}
-                errorText={errorText}
-            />
-        </>
-    );
+    // make sure value is not above maxValue (if given)
+    if (maxValue && targetValue > _.toNumber(maxValue)) {
+      targetValue = maxValue;
+    }
+
+    // TODO: do we want to prevent the unlikely case somebody deliberately or accidentally puts an 'e' for an exponential number?!
+    event.target.value = targetValue;
+
+    setValue(targetValue);
+  };
+  return (
+    <>
+      <div className={classNames(styles.tokenNumberInputContainer, className)}>
+        <Input className={styles.input} type={'number'} value={value} onChange={onChange} {...props} />
+        <span className={styles.eventTokenLabel}>EVNT</span>
+      </div>
+      <ErrorHint className={styles.tokenNumberErrorHint} errorText={errorText} />
+    </>
+  );
 };
 
 export default TokenNumberInput;
