9a874c10e8b2553fa7ecc57688f627da6ed8e518
tolmany <jollydev0821@gmail.com>
2021-07-16 10:14:47 +0200

    Login: Add Terms & Conditions Content

--- src/screens/TermsAndConditions/index.js
+++ src/screens/TermsAndConditions/index.js
@@ -4,22 +4,48 @@ import LegalText          from '../../components/LegalText';
 const TermsAndConditions = () => {
     return (
         <div>
-            <HeaderWithHeadline headline={'Terms and Conditions'} />
+            <HeaderWithHeadline headline={'WALLFAIR.IO PRODUCT LAUNCH EVENT COMPETITION TERMS AND CONDITIONS'} />
             <LegalText>
-                <strong>
-                    1. Lorem ipsum dolor sit amet
-                </strong>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-                dolor
-                sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-                ipsum dolor sit amet.
-                <strong>
-                    2. Lorem ipsum dolor sit amet
-                </strong>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-                dolor
-                sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-                ipsum dolor sit amet.
+<section>
+                <ol><li>	Wallfair.io is conducting the online competition dedicated to the platform launch on July 18, 2021 (hereinafter „Promotion“).
+                </li><li>	By entering the promotion, entrants confirm that they have read and agree to be bound by these terms and conditions and all rules and guidelines applicable to the use of Wallfair.io’s online and social media presence including the Terms & Conditions and Privacy Policy.
+                </li><li>	The Promoter is Rosi Development GmbH, Weidenbornstrasse 8a, 65189 Wiesbaden, the owner of <a href="http://www.wallfair.io">www.wallfair.io</a> and all IP related hereto.
+                </li><li>	Conditions:
+                    <ul>
+                <li>	To enter this promotion, entrants must participate in the product launch event on July 18, 2021 starting at 6 p.m. CEST.
+                </li><li>	Entrants must be aged 18 years or over.
+                </li><li>	This promotion is not open to any employees of the Promoter or any of their affiliate companies, their families, agents or anyone else connected with this promotion.
+                </li><li>	One entry is allowed per person. Entries submitted on behalf of another person will not be accepted and joint submissions are not allowed. Incomplete, illegible, misdirected or late entries will not be accepted.
+                </li><li>	If applicable, copyright in all entries shall belong to the Promoter.
+                </li><li>	No purchase is necessary, however internet access is required.
+                </li><li>	The winner will be determined based on the amount of EVNT tokens collected and accumulated in the online account of the entrant by the time the launch event ends (9 p.m. CEST on July 18, 2021)
+                    </li></ul></li><li>	The promotion is open to entries until 59 CEST on July 17,
+                </li><li>Prize:
+                <ul><li>	The promoter is offering the entrant with the most EVNT tokens accumulated the chance to win a prize of US$ 10.000 to be paid out to the winner’s cryptowallet in USDT. Complete responsibility for the proper storage and spending of the amount remains with the winner upon finalization of the transfer into the winner’s cryptowallet.
+                </li><li>	The prize is non-refundable, non-transferable and non-exchangeable and there is no alternative offered. The Promoter reserves the right to offer an alternative prize of equal or greater value.
+                </li><li>	The winner agrees to provide the information required by the Promoter for the completion of the transfer of the winnings and proper presentation of the final result in due order, specifically, the winner’s name, Instagram account (if available) and cryptowallet address.</li></ul></li>
+                                <li>	Entrants agree to be bound by the decisions of the Promoter, which are final in all matters relating to the promotion. No correspondence will be entered into in respect of the Promoter’s decisions.
+
+                </li><li>	The Promoter does not accept responsibility for network, computer or software failures of any kind and has no responsibility for lost, delayed or misdirected entries. The Promoter does not accept any responsibility for any infringement of any third party intellectual property rights caused by entrants entering this promotion.
+
+                </li><li>	Nothing in these terms excludes the Promoter’s liability for personal injury caused by its negligence or for fraud, or any other liability that the Promoter may not exclude by law.  Subject to that, the Promoter and its associated companies will not be liable for any loss (including, without limitation, indirect, special or consequential loss or loss of profits) expense or damage which is suffered or sustained in connection with this promotion or accepting or using any prize, including any loss incurred as a result of any delay and/or failure to perform any obligation to the winner or other entrants that is caused by:
+                <ul>
+                <li>	technical problems of any kind which may limit or prevent any person's participation in the promotion;
+                </li><li>	any loss or damage arising from or in connection with the allocation or enjoyment of a prize;
+                </li><li>	any act or omission of any third party, particularly any third party involved in the provision of services relevant to the prize; or
+                </li><li>	any other events beyond the Promoter's control that may cause the promotion to be disrupted or corrupted or may lead to loss of, or damage to, the winner or to the prize.</li>
+                </ul>
+                </li><li>	If any of these terms and conditions are found by a court or regulator to be invalid or unenforceable the remaining other provisions shall continue to apply.
+
+                </li><li>	This promotion is in no way sponsored, endorsed or administered by or associated with Facebook, Instagram or any other social media channel.  By entering, entrants acknowledge that Facebook and Instagram do not bear any responsibility for this promotion and, to the maximum extent permitted by law, entrants release Facebook and Instagram from any liability whatsoever in connection with this promotion. All information entrants provide when they enter the promotion is provided to the Promoter and not to Facebook, Instagram or any other social media channel.
+
+                </li><li>	Subject to the winner’s rights under applicable data protection legislation, the Promoter shall be entitled to use and feature the name of the Pamper Hamper Sleep Competition winner for publicity purposes and winners agree that they will participate in any reasonable publicity arranged by the Promoter or its agencies.
+
+                </li><li>	These terms and conditions shall be governed by and construed in accordance with the laws of Germany and the parties agree that any dispute or action arising under or in connection with these terms and conditions shall be subject to the exclusive jurisdiction of the Courts of the Federal Republic of Germany.
+                </li></ol>
+
+                Wiesbaden, July 1, 2021
+</section>
             </LegalText>
         </div>
     );
