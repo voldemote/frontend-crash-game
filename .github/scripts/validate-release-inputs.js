9f59c80ea9059cbe517ba691fd6f52819c452001
tolmany <jollydev0821@gmail.com>
2021-08-04 14:14:37 +0200

    Update github actions with new functions

--- .github/scripts/validate-release-inputs.js
+++ .github/scripts/validate-release-inputs.js
@@ -1,4 +1,9 @@
 if (['staging', 'production'].includes(process.env.stage)) {
     console.log("stage must be either 'staging' or 'production'");
     process.exit(1);
-  }
+}
+
+if (['dev', 'prod', 'latest'].includes(process.env.version)) {
+    console.log("version must follow semantic syntax like 1.x.x, 'prod', 'dev' and 'latest' are not valid inputs");
+    process.exit(1);
+}
