19ab5bbe4be3b9254e7a9dd72a31dff3017316d5
tolmany <jollydev0821@gmail.com>
2021-08-04 15:18:02 +0200

    fix variable assignment on github action

--- .github/scripts/validate-release-inputs.js
+++ .github/scripts/validate-release-inputs.js
@@ -3,7 +3,7 @@ if (!['staging', 'production'].includes(process.env.stage)) {
     process.exit(1);
 }
 
-if (!['dev', 'prod', 'latest'].includes(process.env.version)) {
+if (['dev', 'prod', 'latest'].includes(process.env.version)) {
     console.log("version must follow semantic syntax like 1.x.x, 'prod', 'dev' and 'latest' are not valid inputs, but it was ", process.env.version);
     process.exit(1);
 }
