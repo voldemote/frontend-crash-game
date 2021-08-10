e6f47a3cf24ec0d8c3a7b52a1f342b68f1be1025
tolmany <jollydev0821@gmail.com>
2021-08-10 20:56:45 +0200

    - prevent join popup showing after user is authenticated

--- src/store/sagas/authentication.js
+++ src/store/sagas/authentication.js
@@ -5,7 +5,7 @@ import Routes                       from '../../constants/Routes';
 import { AuthenticationActions }    from '../actions/authentication';
 import { EventActions }             from '../actions/event';
 import { push }                     from 'connected-react-router';
-import { put, call, select, delay } from 'redux-saga/effects';
+import { put, call, select, delay, cancel } from 'redux-saga/effects';
 import { UserActions }              from '../actions/user';
 import { BetActions }               from '../actions/bet';
 import { TransactionActions }       from '../actions/transaction';
@@ -190,6 +190,7 @@ const authenticationSucceeded = function* (action) {
     const userId    = yield select(state => state.authentication.userId);
 
     if (authState === AuthState.LOGGED_IN) {
+        yield cancel(firstSignUpPopup);
         yield put(UserActions.fetch({ userId, forceFetch: true }));
         yield put(EventActions.fetchAll());
         yield put(AuthenticationActions.fetchReferrals());
@@ -243,6 +244,7 @@ const refreshImportantData = function* () {
     const authState = yield select(state => state.authentication.authState);
 
     if (authState === AuthState.LOGGED_IN) {
+        yield cancel(firstSignUpPopup);
         yield put(UserActions.fetch({ forceFetch: true }));
         yield put(EventActions.fetchAll());
         yield put(AuthenticationActions.fetchReferrals());
@@ -254,19 +256,18 @@ const refreshImportantData = function* () {
     }
 };
 
-const firstSignUpPopup = function* () {
+const firstSignUpPopup = function* (options) {
     const authState = yield select(state => state.authentication.authState);
 
     if (authState === AuthState.LOGGED_OUT) {
-        yield delay(60 * 1000);
+        yield delay((options.duration ? options.duration : 1) * 60 * 1000);
         yield put(PopupActions.show({
-            popupType: PopupTheme.signUpNotificationFirst
+            popupType: options.last ? PopupTheme.signUpNotificationSecond : PopupTheme.signUpNotificationFirst
         }));
 
-        yield delay(300 * 1000);
-        yield put(PopupActions.show({
-            popupType: PopupTheme.signUpNotificationSecond
-        }));
+        if(!options.last) {
+            yield call(firstSignUpPopup, { last: true, duration: 5 })
+        }
     }
 }
 
