40bcad83ef96358ba21b62ed6ac4aaaee630748b
tolmany <jollydev0821@gmail.com>
2021-08-11 22:11:16 +0200

    event tags in slider, time left counter

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,17 @@
-import _                from 'lodash';
-import LiveBadge        from '../LiveBadge';
-import { useState }     from 'react';
-import styles           from './styles.module.scss';
-import Link             from '../../components/Link';
-import { connect }      from 'react-redux';
-import TwitchEmbedVideo from '../TwitchEmbedVideo';
-import Routes           from '../../constants/Routes';
-import EventBetPillList from '../EventBetPillList';
-import BetState         from '../BetView/BetState';
-import moment           from 'moment';
+import _                 from 'lodash';
+import LiveBadge         from '../LiveBadge';
+import { useState }      from 'react';
+import styles            from './styles.module.scss';
+import Link              from '../../components/Link';
+import { connect }       from 'react-redux';
+import TwitchEmbedVideo  from '../TwitchEmbedVideo';
+import Routes            from '../../constants/Routes';
+import EventBetPillList  from '../EventBetPillList';
+import BetState          from '../BetView/BetState';
+import moment            from 'moment';
 import CoverFlowCarousel from '../CoverFlowCarousel';
+import TimeLeftCounter   from '../TimeLeftCounter';
+import classNames        from 'classnames';
 
 const Header = ({ events }) => {
 
@@ -24,6 +26,44 @@ const Header = ({ events }) => {
         );
     };
 
+    const sortTrades = (trades) => {
+        return trades.sort((a, b) => {
+            const aState        = _.get(a, 'status');
+            const bState        = _.get(b, 'status');
+            const getStateValue = (state) => {
+                switch (state) {
+                    case BetState.active:
+                        return 5;
+                    case BetState.closed:
+                        return 4;
+                    case BetState.resolved:
+                        return 3;
+                    case BetState.canceled:
+                        return 2;
+                }
+
+                return 1;
+            };
+
+            if (aState === bState) {
+                const aEndDate = moment(_.get(a, 'endDate'));
+                const bEndDate = moment(_.get(b, 'endDate'));
+
+                if (aEndDate.isBefore(bEndDate)) {
+                    return 1;
+                }
+
+                if (bEndDate.isBefore(aEndDate)) {
+                    return -1;
+                }
+
+                return 0;
+            }
+
+            return getStateValue(bState) - getStateValue(aState);
+        });
+    }
+
     return (
         <div>
             <div className={styles.header}>
@@ -35,44 +75,9 @@ const Header = ({ events }) => {
                         const startDate   = moment(_.get(event, 'date'));
                         const endDate     = moment(_.get(event, 'endDate'));
                         const currentDate = moment();
-                        const isLive      = currentDate.isBetween(startDate, endDate);
-                        const trades      = _.clone(_.get(event, 'bets', []));
+                        const isLive      = true;//currentDate.isBetween(startDate, endDate);
+                        const trades      = sortTrades(_.clone(_.get(event, 'bets', [])));
 
-                        trades.sort((a, b) => {
-                            const aState        = _.get(a, 'status');
-                            const bState        = _.get(b, 'status');
-                            const getStateValue = (state) => {
-                                switch (state) {
-                                    case BetState.active:
-                                        return 5;
-                                    case BetState.closed:
-                                        return 4;
-                                    case BetState.resolved:
-                                        return 3;
-                                    case BetState.canceled:
-                                        return 2;
-                                }
-
-                                return 1;
-                            };
-
-                            if (aState === bState) {
-                                const aEndDate = moment(_.get(a, 'endDate'));
-                                const bEndDate = moment(_.get(b, 'endDate'));
-
-                                if (aEndDate.isBefore(bEndDate)) {
-                                    return 1;
-                                }
-
-                                if (bEndDate.isBefore(aEndDate)) {
-                                    return -1;
-                                }
-
-                                return 0;
-                            }
-
-                            return getStateValue(bState) - getStateValue(aState);
-                        });
                         const currentTrade   = _.head(trades);
                         const currentTradeId = _.get(currentTrade, '_id');
 
@@ -98,6 +103,13 @@ const Header = ({ events }) => {
                                         <span className={styles.title}>
                                             {event.name}
                                         </span>
+                                        <div className={styles.tagList}>
+                                            {event.tags.map(({name}) => (
+                                                <span className={styles.tag}>
+                                                    #{name.toLowerCase()}
+                                                </span>
+                                            ))}
+                                        </div>
                                         <div>
                                             <Link
                                                 to={Routes.getRouteWithParameters(
@@ -115,6 +127,13 @@ const Header = ({ events }) => {
                                             </Link>
                                         </div>
                                     </div>
+                                </div>
+                                <div className={classNames(
+                                    styles.timeLeftCounterContainer,
+                                    currentSlideIndex === eventIndex && styles.showTimeLeftCounter
+                                )}>
+                                    <span className={styles.timeLeftCaption}>Event ends in:</span>
+                                    <TimeLeftCounter endDate={endDate} />
                                 </div>
                             </div>
                         );
