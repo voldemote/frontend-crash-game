71f646234f6ef2289b13eaa41b688c27c8d6bf4a
tolmany <jollydev0821@gmail.com>
2021-08-20 13:25:38 +0200

    add frogotten type for category gfx

--- src/screens/Events/index.js
+++ src/screens/Events/index.js
@@ -38,6 +38,7 @@ const Events = () => {
             value: 'crypto',
             image: SportsImage,
             isActive: false,
+            type: 'icon',
         },
         {
             value: 'other',
