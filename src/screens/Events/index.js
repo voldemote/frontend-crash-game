a22e49acf131d83f7e5fc30f58c1de54a38aa7e0
tolmany <jollydev0821@gmail.com>
2021-08-23 18:02:53 +0200

    fix styling for categories, missing icons added

--- src/screens/Events/index.js
+++ src/screens/Events/index.js
@@ -7,6 +7,7 @@ import OtherImage from '../../data/icons/events-other-icon.svg';
 import PoliticsImage from '../../data/icons/events-politics-icon.svg';
 import CelebritiesImage from '../../data/icons/events-celebrities-icon.svg';
 import AllImage from '../../data/icons/events-all-icon.svg';
+import CryptoImage from '../../data/icons/events-crypto-icon.svg';
 
 const Events = () => {
     const [categories, setCategories] = useState([
@@ -36,16 +37,16 @@ const Events = () => {
         },
         {
             value: 'crypto',
-            image: SportsImage,
+            image: CryptoImage,
             isActive: false,
             type: 'icon',
         },
-        {
-            value: 'other',
-            image: OtherImage,
-            isActive: false,
-            type: 'icon',
-        },
+        // {
+        //     value: 'other',
+        //     image: OtherImage,
+        //     isActive: false,
+        //     type: 'icon',
+        // },
     ]);
 
     return (
