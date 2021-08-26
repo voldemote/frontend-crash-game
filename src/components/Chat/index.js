8c25f42c7bbb7e421b5a46052b375128b40798db
tolmany <jollydev0821@gmail.com>
2021-08-26 08:43:14 +0200

    MVP2-113: Remove hardcoded messages from chat

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -134,18 +134,11 @@ const Chat = ({ className, inputClassName, messagesClassName, userId, userName, 
     );
 };
 
-const messages2 = [
-    { id: 1, type: 'chatMessage', user: { name: 'ivan1989', profilePicture: null }, userId: 1, date: '', message: 'ivan crashed at 3.40x' },
-    { id: 2, type: 'chatMessage', user: { name: 'Dexter', profilePicture: null }, userId: 2, date: '', message: 'Dexter crashed at 7.90x' },
-    { id: 3, type: 'chatMessage', user: { name: 'Nekorandomime', profilePicture: null }, userId: 3, date: '', message: 'Nekorandomime crashed at 7.90x' }
-];
-
 const mapStateToProps = (state, ownProps) => {
     return {
         userId:    state.authentication.userId,
         userName:  state.authentication.name,
-        messages: messages2,
-        // messages:  _.get(state, ['chat', 'messagesByEvent', _.get(ownProps.event, '_id')], []),
+        messages:  _.get(state, ['chat', 'messagesByEvent', _.get(ownProps.event, '_id')], []),
         connected: state.websockets.connected,
     };
 };
