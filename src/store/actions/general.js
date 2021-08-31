8e4c0b4e7d0435b499d08d3222b35f11c4a08e7c
tolmany <jollydev0821@gmail.com>
2021-08-31 11:04:37 +0200

    MVP2-150: Move balance to wallet drawer

--- src/store/actions/general.js
+++ src/store/actions/general.js
@@ -1,18 +1,26 @@
 export const GeneralTypes = {
   SET_GLOBAL_DRAWER: 'General/SET_GLOBAL_DRAWER',
   SET_EDIT_PROFILE_VISIBLE: 'General/SET_EDIT_PROFILE_VISIBLE',
+  SET_MY_TRADES_VISIBLE: 'General/SET_MY_TRADES_VISIBLE',
 };
 
 const setDrawer = payload => ({
   type: GeneralTypes.SET_GLOBAL_DRAWER,
   payload,
 });
+
 const setEditProfileVisible = payload => ({
   type: GeneralTypes.SET_EDIT_PROFILE_VISIBLE,
   payload,
 });
 
+const setMyTradesVisible = (visible) => ({
+  type: GeneralTypes.SET_MY_TRADES_VISIBLE,
+  visible,
+});
+
 export const GeneralActions = {
   setDrawer,
   setEditProfileVisible,
+  setMyTradesVisible,
 };
