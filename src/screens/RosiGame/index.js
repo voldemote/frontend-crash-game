7a726079ee3d30146b2a757b1ef72b625b6ae529
tolmany <jollydev0821@gmail.com>
2021-09-06 14:12:12 +0200

    Fix rosi game chat styles

--- src/screens/RosiGame/index.js
+++ src/screens/RosiGame/index.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import * as Api from 'api/crash-game';
 import { useDispatch } from 'react-redux';
-import Link from 'components/Link';
 import Grid from '@material-ui/core/Grid';
 import BaseContainerWithNavbar from 'components/BaseContainerWithNavbar';
-// import PlaceBet from 'components/PlaceBet';
+import PlaceBet from 'components/PlaceBet';
+import BackLink from 'components/BackLink';
 import LastCrashes from 'components/LastCrashes';
 import GameAnimation from 'components/RosiGameAnimation';
 import GameBets from 'components/GameBets';
@@ -12,7 +12,6 @@ import Chat from 'components/Chat';
 import { ROSI_GAME_EVENT_ID } from 'constants/RosiGame';
 import { RosiGameActions } from 'store/actions/rosi-game';
 import { winners, inGameBets } from './fakeData';
-import { ReactComponent as ArrowIcon } from '../../data/icons/arrow-left.svg';
 import styles from './styles.module.scss';
 
 const rosiGameEvent = { _id: ROSI_GAME_EVENT_ID };
@@ -30,17 +29,19 @@ const RosiGame = () => {
     <BaseContainerWithNavbar withPaddingTop={true}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <div>
-            <Link to="/games" className={styles.backLink}>
-              <ArrowIcon />
-              <h2>Rosi Game</h2>
-            </Link>
-          </div>
+          <BackLink to="/games" text="Rosi Game" />
           <LastCrashes />
-          <GameAnimation />
+          <Grid container spacing={2}>
+            <Grid item md={9}>
+              <GameAnimation />
+            </Grid>
+            <Grid item md={3}>
+              <PlaceBet />
+            </Grid>
+          </Grid>
           <Grid container spacing={2}>
             <Grid item md={4}>
-              <Chat event={rosiGameEvent} />
+              <Chat event={rosiGameEvent} className={styles.chatContainer} />
             </Grid>
             <Grid item md={4}>
               <GameBets
@@ -59,9 +60,6 @@ const RosiGame = () => {
             </Grid>
           </Grid>
         </div>
-        {/* <div className={styles.sidebar}>
-          <PlaceBet />
-        </div> */}
       </div>
     </BaseContainerWithNavbar>
   );
