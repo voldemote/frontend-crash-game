b8a5aee96a831247adb57f3ca87587d774422157
tolmany <jollydev0821@gmail.com>
2021-09-07 16:05:26 +0200

    Add more secrets to github actions pipeline

--- .github/scripts/generate-spec.js
+++ .github/scripts/generate-spec.js
@@ -4,8 +4,27 @@ const fs = require('fs');
 let yml = fs
   .readFileSync(path.resolve(__dirname, `spec-${process.env.stage}.yml`))
   .toString();
-console.log(
-  yml
-    .replace('$VERSION', process.env.version)
-    .replace('$REACT_APP_NEWS_API_KEY', process.env.REACT_APP_NEWS_API_KEY)
-);
+
+if (process.env.stage == 'STAGING') {
+  console.log(
+    yml
+      .replace('$VERSION', process.env.version)
+      .replace('$REACT_APP_NEWS_API_KEY', process.env.REACT_APP_NEWS_API_KEY)
+      .replace('$REACT_APP_NEWS_API_KEY', process.env.STAGING_BACKEND_URL)
+      .replace(
+        '$REACT_APP_NEWS_API_KEY',
+        process.env.STAGING_CRASH_GAME_BACKEND_URL
+      )
+  );
+} else if (process.env.stage == 'PRODUCTIVE') {
+  console.log(
+    yml
+      .replace('$VERSION', process.env.version)
+      .replace('$REACT_APP_NEWS_API_KEY', process.env.REACT_APP_NEWS_API_KEY)
+      .replace('$REACT_APP_NEWS_API_KEY', process.env.PRODUCTIVE_BACKEND_URL)
+      .replace(
+        '$REACT_APP_NEWS_API_KEY',
+        process.env.PRODUCTIVE_CRASH_GAME_BACKEND_URL
+      )
+  );
+}
