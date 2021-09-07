02ec8247b2c51988e9ad912173df235a4a62bfd9
tolmany <jollydev0821@gmail.com>
2021-09-07 19:12:13 +0200

    Fix page reload issue

--- src/components/JoinPopup/index.js
+++ src/components/JoinPopup/index.js
@@ -1,25 +1,9 @@
 import _ from 'lodash';
-
-import React from 'react';
 import classNames from 'classnames';
 import styles from './styles.module.scss';
-import { connect } from 'react-redux';
-import { PopupActions } from '../../store/actions/popup';
-import { LOGGED_IN } from 'constants/AuthState';
-import Routes from 'constants/Routes';
-import { useHistory } from 'react-router-dom';
-import { useEffect } from 'react';
 import Authentication from '../Authentication';
 
-const JoinPopup = ({ closed, user, hidePopup, authState }) => {
-  const history = useHistory();
-
-  useEffect(() => {
-    if (authState === LOGGED_IN) {
-      history.push(Routes.home);
-    }
-  }, []);
-
+const JoinPopup = () => {
   return (
     <div className={styles.welcomeContainer}>
       <div className={styles.welcomeContentContainer}>
@@ -33,18 +17,4 @@ const JoinPopup = ({ closed, user, hidePopup, authState }) => {
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    authState: state.authentication.authState,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    hidePopup: () => {
-      dispatch(PopupActions.hide());
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(JoinPopup);
+export default JoinPopup;
