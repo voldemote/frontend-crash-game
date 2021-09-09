c13e15878af8447d880736efeb990d2c964c8bfb
tolmany <jollydev0821@gmail.com>
2021-09-09 17:57:39 +0800

    feat: MVP2-196 send event evaluate mail

--- src/api/index.js
+++ src/api/index.js
@@ -215,6 +215,19 @@ const getEventHistoryChartData = (betId, params = {}) => {
   });
 };
 
+const sendEventEvaluate = (betQuestion, rating, comment) => {
+  const payload = {
+    bet_question: betQuestion,
+    rating,
+    comment,
+  };
+  return Api.post('http://localhost:8000/api/event/evaluate', {
+    payload,
+  }).catch(error => {
+    console.log('[API Error] called: sendEventEvaluate', error);
+  });
+};
+
 export {
   Api,
   createBet,
@@ -239,4 +252,5 @@ export {
   resendEmailVerification,
   getTags,
   getEventHistoryChartData,
+  sendEventEvaluate,
 };
