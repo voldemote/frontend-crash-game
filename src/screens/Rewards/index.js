61023a7e9303a17244799b7c610b06e015831eb5
tolmany <jollydev0821@gmail.com>
2021-09-13 18:19:10 +0200

    loading card, no open questions card, responsive fixes, custom radio indicator

--- src/screens/Rewards/index.js
+++ src/screens/Rewards/index.js
@@ -18,7 +18,10 @@ import {
 import PopupTheme from 'components/Popup/PopupTheme';
 import { PopupActions } from '../../store/actions/popup';
 
+import CelebrationBadge from '../../data/images/confetti-winnings-badge.svg';
+
 import styles from './styles.module.scss';
+import { TOKEN_NAME } from 'constants/Token';
 
 const RewardCard = ({
   rewardAmount = 0,
@@ -50,14 +53,12 @@ const RewardCard = ({
           disabled={isDisabled}
           onClick={onClick}
         >
-          <div>
-            <span>{buttonText}</span>
-            <Icon
-              className={styles.iconRightSide}
-              iconType={IconType.arrowSmallRight}
-              iconTheme={IconTheme.black}
-            />
-          </div>
+          <span>{buttonText}</span>
+          <Icon
+            className={styles.iconRightSide}
+            iconType={IconType.arrowSmallRight}
+            iconTheme={IconTheme.black}
+          />
         </Button>
       )}
     </div>
@@ -150,10 +151,12 @@ const LotteryGame = ({
   );
   const [checkedOption, setCheckedOption] = useState(null);
   const [complete, setCompleted] = useState(isComplete);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getRewardsQuestions().then(res => {
       setQuestions(res.data);
+      setLoading(false);
     });
   }, []);
 
@@ -191,9 +194,28 @@ const LotteryGame = ({
     setCheckedOption(value);
   };
 
+  if (loading) {
+    return (
+      <div className={classNames(styles.card, styles.cardCenter)}>
+        <div className={styles.lotteryGameContent}>
+          <span className={styles.loader}></span>
+          <p>Looking for open lottery games...</p>
+        </div>
+      </div>
+    );
+  }
+
   if (!questions.length) {
-    // TODO loading layout
-    return null;
+    return (
+      <div className={classNames(styles.card, styles.cardCenter)}>
+        <div className={styles.lotteryGameContent}>
+          <p>
+            You've completed all existing lottery questions. <br /> Come back
+            later for more.
+          </p>
+        </div>
+      </div>
+    );
   }
 
   if (complete) {
@@ -210,6 +232,12 @@ const LotteryGame = ({
 
   return (
     <div className={styles.card}>
+      {!isSubmitDisabled && (
+        <div className={styles.celebrationBadge}>
+          <img src={CelebrationBadge} alt="celebration" />
+          <span>180 {TOKEN_NAME}</span>
+        </div>
+      )}
       <div>
         <span className={styles.questionNumberLabel}>
           {activeQuestionNumber + 1}/{questions.length}
@@ -222,22 +250,24 @@ const LotteryGame = ({
           {activeQuestion.questions.map(({ index, name }) => {
             const elementId = `${activeQuestion._id}_${index}`;
             return (
-              <label
-                key={index}
-                for={elementId}
-                onClick={() => setOption(index)}
-                className={styles.radioButton}
-                checked={checkedOption === index}
-              >
+              <>
                 <input
                   type="radio"
                   id={elementId}
-                  name={name}
+                  name={activeQuestion._id}
                   value={index}
-                  checked={checkedOption === index}
+                  className={styles.checkbox}
+                  onChange={() => setOption(index)}
                 />
-                {name}
-              </label>
+                <label
+                  key={index}
+                  htmlFor={elementId}
+                  className={styles.radioButton}
+                  checked={checkedOption === index}
+                >
+                  {name}
+                </label>
+              </>
             );
           })}
         </div>
@@ -284,11 +314,7 @@ const Rewards = ({ user, showPopup }) => {
       <div className={styles.container}>
         <div className={styles.header}>
           <div className={styles.rewardsInfo}>
-            <h3>
-              Activation
-              <br />
-              Games
-            </h3>
+            <h3>Activation Games</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam
               feugiat, turpis at pulvinar vulputate, erat libero tristique
