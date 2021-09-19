0d3ddb7b2e1feafeb4223bd054025ef31315819f
tolmany <jollydev0821@gmail.com>
2021-09-19 19:26:34 +0300

    added cover stream

--- src/api/index.js
+++ src/api/index.js
@@ -301,6 +301,12 @@ const createEventFromYoutubeUrl = data => {
     .catch(error => ({ error: error.message }));
 };
 
+const getCoverStream = () => {
+  return Api.get(ApiUrls.API_EVENT_GET_COVER_STREAM)
+    .then(response => ({ response }))
+    .catch(error => ({ error: error.response.data }));
+};
+
 export {
   Api,
   createBet,
@@ -336,4 +342,5 @@ export {
   updateUserPreferences,
   createEventFromTwitchUrl,
   createEventFromYoutubeUrl,
+  getCoverStream,
 };
