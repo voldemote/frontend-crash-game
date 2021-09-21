c5a91676ee5a9ce578bbe2dd0bce9f01bf693313
tolmany <jollydev0821@gmail.com>
2021-09-21 14:00:32 +0200

    Remove Docker stuff

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-defaultversion=dev
-VERSION=${1:-$defaultversion}
-
-npm run build
-doctl registry login
-docker build . -t registry.digitalocean.com/wallfair/frontend:$VERSION
-docker push registry.digitalocean.com/wallfair/frontend:$VERSION
