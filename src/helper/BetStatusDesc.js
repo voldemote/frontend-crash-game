812640d9b947a4f06bec0982037b5bfdc6191ffc
tolmany <jollydev0821@gmail.com>
2021-09-21 15:55:09 +0200

    MVP2-249 - Bet boxes labels update

--- src/helper/BetStatusDesc.js
+++ src/helper/BetStatusDesc.js
@@ -1,7 +1,7 @@
 export const BET_STATUS_DESCRIPTIONS = {
   active: 'Bet will end in',
-  closed: 'Bet was closed at',
+  closed: 'Bet was closed on',
   upcoming: 'Bet will start in',
-  resolved: 'Bet was resolved at',
-  canceled: 'Bet was canceled at',
+  resolved: 'Bet was resolved on',
+  canceled: 'Bet was canceled on',
 };
