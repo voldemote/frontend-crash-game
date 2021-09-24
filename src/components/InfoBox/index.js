7e2932434c3fcefd8947cf1f55d5b385a50bfea9
tolmany <jollydev0821@gmail.com>
2021-09-24 12:34:34 +0200

    created infobox component + adjusted bet view to allow the infobox content to appear correctly (#340)

---
+++ src/components/InfoBox/index.js
@@ -0,0 +1,56 @@
+import classNames from 'classnames';
+import { useOutsideClick } from 'hooks/useOutsideClick';
+import React, { useCallback, useState } from 'react';
+import Icon from '../Icon';
+import IconType from '../Icon/IconType';
+import styles from './styles.module.scss';
+
+const POSITIONS = {
+  topLeft: 'topLeft',
+  topRight: 'topRight',
+  bottomLeft: 'bottomLeft',
+  bottomRight: 'bottomRight',
+};
+
+const InfoBox = ({ children, position = POSITIONS.topLeft }) => {
+  const [showContent, setShowContent] = useState(false);
+
+  const element = useOutsideClick(() => {
+    setShowContent(false);
+  });
+
+  const renderInfoIcon = () => {
+    return (
+      <Icon
+        className={styles.infoIcon}
+        iconType={IconType.info}
+        onClick={onHandleClick}
+      />
+    );
+  };
+
+  const onHandleClick = useCallback(
+    event => {
+      setShowContent(!showContent);
+    },
+    [showContent]
+  );
+
+  return (
+    <div ref={element} className={styles.infoBoxContainer}>
+      {renderInfoIcon()}
+      {showContent && (
+        <div
+          className={classNames(
+            styles.infoBoxContentContainer,
+            styles[position]
+          )}
+        >
+          <div className={styles.infoBoxContent}>{children}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfoBox;
