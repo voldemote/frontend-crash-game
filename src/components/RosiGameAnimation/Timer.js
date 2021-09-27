0ea6d58cbd8f75ef66bccc00f739383598251ba7
tolmany <jollydev0821@gmail.com>
2021-09-27 11:29:24 +0300

    Rosi Game: slow down counter animation

--- src/components/RosiGameAnimation/Timer.js
+++ src/components/RosiGameAnimation/Timer.js
@@ -1,53 +1,31 @@
 import { useEffect, useState } from 'react';
 
+// Crash factor = (elapsed time) * TIME_TO_FACTOR_RATIO
+const TIME_TO_FACTOR_RATIO = 0.5; // 1s = 0.5x
+
 const Timer = ({ pause, startTimeMs }) => {
   const startTime = new Date(startTimeMs);
-  const [seconds, setSeconds] = useState(startTime.getSeconds());
-  const [ms, setMs] = useState(startTime.getMilliseconds());
+  const [elapsed, setElapsed] = useState(startTime.getTime());
+  const elapsedSeconds = (elapsed * TIME_TO_FACTOR_RATIO) / 1000;
+  const wholePart = Math.trunc((elapsed * TIME_TO_FACTOR_RATIO) / 1000);
+  const decimalPart =
+    Math.trunc((elapsedSeconds - Math.floor(elapsedSeconds)) * 10) * 10;
 
   useEffect(() => {
-    let timeoutHandle;
+    const interval = setInterval(() => {
+      if (pause) {
+        return;
+      }
 
-    if (pause) {
-      clearTimeout(timeoutHandle);
-      return;
-    }
+      setElapsed(e => e + 10);
+    }, 10);
 
-    if (seconds < 60) {
-      timeoutHandle = setTimeout(() => {
-        if (pause) return;
-        setSeconds(prevSeconds => prevSeconds + 1);
-      }, 1000);
-    } else {
-      setSeconds(0);
-    }
-
-    return () => clearTimeout(timeoutHandle);
-  }, [seconds, pause]);
-
-  useEffect(() => {
-    let timeoutHandle;
-
-    if (pause) {
-      clearTimeout(timeoutHandle);
-      return;
-    }
-
-    if (ms < 100) {
-      timeoutHandle = setTimeout(() => {
-        if (pause) return;
-        setMs(prevMs => prevMs + 10);
-      }, 100);
-    } else {
-      setMs(0);
-    }
-
-    return () => clearTimeout(timeoutHandle);
-  }, [ms, pause]);
+    return () => clearInterval(interval);
+  }, [pause]);
 
   return (
     <span>
-      {seconds}.{ms === 0 ? '00' : ms}
+      {wholePart}.{decimalPart === 0 ? '00' : decimalPart}
     </span>
   );
 };
