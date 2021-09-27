ab038162cb003cb593c2dce796da4f6e2a7fdcbb
tolmany <jollydev0821@gmail.com>
2021-09-27 14:52:52 +0300

    Rosi Game: count milliseconds in multiple of 5 increments

--- src/components/RosiGameAnimation/Timer.js
+++ src/components/RosiGameAnimation/Timer.js
@@ -4,13 +4,19 @@ import { useEffect, useState } from 'react';
 const TIME_TO_FACTOR_RATIO = 0.5; // 1s = 0.5x
 const START_FACTOR = 1;
 
+function round5(x) {
+  return Math.floor(x / 5) * 5;
+}
+
 const Timer = ({ pause, startTimeMs }) => {
   const startTime = new Date(startTimeMs);
   const [elapsed, setElapsed] = useState(startTime.getTime());
   const elapsedSeconds = (elapsed * TIME_TO_FACTOR_RATIO) / 1000 + START_FACTOR;
   const wholePart = Math.trunc(elapsedSeconds);
-  const decimalPart =
-    Math.trunc((elapsedSeconds - Math.floor(elapsedSeconds)) * 10) * 10;
+  // round up decimal part to the previous multiple of 5. So it would count 40, 45, 50, 55 ...
+  const decimalPart = round5(
+    (elapsedSeconds - Math.floor(elapsedSeconds)) * 100
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,7 +32,7 @@ const Timer = ({ pause, startTimeMs }) => {
 
   return (
     <span>
-      {wholePart}.{decimalPart === 0 ? '00' : decimalPart}
+      {wholePart}.{decimalPart < 10 ? `0${decimalPart}` : decimalPart}
     </span>
   );
 };
