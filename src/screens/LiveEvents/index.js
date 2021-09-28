1ba50108bf536bd540b4d4d30b2c329d939b2cb6
tolmany <jollydev0821@gmail.com>
2021-09-28 13:29:08 +0200

    use event categories in streamed page

--- src/screens/LiveEvents/index.js
+++ src/screens/LiveEvents/index.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import BaseContainerWithNavbar from 'components/BaseContainerWithNavbar';
 import EventsContent from 'components/Events/EventsContent';
-import { LIVE_EVENTS_CATEGORIES } from 'constants/EventCategories';
+import { EVENT_CATEGORIES } from 'constants/EventCategories';
 
 const LiveEvents = ({ history }) => {
-  const [categories, setCategories] = useState(LIVE_EVENTS_CATEGORIES);
+  const [categories, setCategories] = useState(EVENT_CATEGORIES);
 
   return (
     <BaseContainerWithNavbar withPaddingTop={true}>
