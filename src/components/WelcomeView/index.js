bac08aca9d995565c4a65a82a0ae787fdfbab36a
tolmany <jollydev0821@gmail.com>
2021-09-29 08:20:36 +0200

    added disclaimer and adjusted welcome popup styling

--- src/components/WelcomeView/index.js
+++ src/components/WelcomeView/index.js
@@ -7,8 +7,18 @@ import { connect } from 'react-redux';
 import Button from '../Button';
 import { PopupActions } from '../../store/actions/popup';
 import { TOKEN_NAME } from '../../constants/Token';
+import { useCallback, useState } from 'react';
 
 const WelcomeView = ({ closed, user, hidePopup }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const handleClickReadMore = useCallback(
+    event => {
+      setReadMore(true);
+    },
+    [setReadMore]
+  );
+
   const renderHeadline = () => {
     const name = _.get(user, 'name');
 
@@ -30,6 +40,37 @@ const WelcomeView = ({ closed, user, hidePopup }) => {
         <span className={styles.welcomeTextText}>
           Refer a friend and get additional 500 {TOKEN_NAME}.
         </span>
+      </div>
+    );
+  };
+
+  const renderAlphaDisclaimer = () => {
+    return (
+      <div className={styles.welcomeTextContainer}>
+        <span className={styles.disclaimer}>
+          Please be aware that the platform you are currently using is based on
+          the virtual WFAIR tokens that have been implemented with the purpose
+          to provide a full impression of the Wallfair gaming experience.{' '}
+          {!readMore && (
+            <span className={styles.readmore} onClick={handleClickReadMore}>
+              READ MORE
+            </span>
+          )}
+        </span>
+
+        {readMore && (
+          <span className={styles.disclaimer}>
+            The platform is currently being optimized on testnet before going
+            live on mainnet. Your token count will be accurately recorded on the
+            internal ledger, which is why you will not be able to trade, sell or
+            stake the tokens outside or extract them from the platform for now,
+            however, we would like to disclose that the blockchain component of
+            the platform will be implemented within the next 3 months (we will
+            inform you accordingly) - thereafter, you will be able to use the
+            token at their full capacity. In the meantime, please enjoy the
+            testnet version!
+          </span>
+        )}
       </div>
     );
   };
@@ -57,6 +98,7 @@ const WelcomeView = ({ closed, user, hidePopup }) => {
       {renderHeadline()}
       {renderWelcomeText()}
       {renderStartTradingButton()}
+      {renderAlphaDisclaimer()}
     </div>
   );
 };
