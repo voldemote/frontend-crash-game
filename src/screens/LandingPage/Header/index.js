3026cf09d7436a4bf854e1c88109e58057c2f6e7
tolmany <jollydev0821@gmail.com>
2021-09-30 16:11:33 +0200

    Added video to landing page and added loop and playsInline props to video player

--- src/screens/LandingPage/Header/index.js
+++ src/screens/LandingPage/Header/index.js
@@ -1,3 +1,4 @@
+import VideoPlayer from 'components/EmbedVideo/VideoPlayer';
 import styles from './styles.module.scss';
 
 const LandingPage = () => {
@@ -5,7 +6,13 @@ const LandingPage = () => {
     <div className={styles.header}>
       <h6 className={styles.subHeading}>Join the ride.</h6>
       <h1 className={styles.mainHeading}>How it works for Normies</h1>
-      {/* <div className={styles.video} /> */}
+      <div className={styles.video}>
+        <VideoPlayer
+          video="https://files.wallfair.io/wallfair.mp4"
+          controls={true}
+          loop={true}
+        />
+      </div>
     </div>
   );
 };
