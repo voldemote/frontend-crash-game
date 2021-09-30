5ca41dc3aa8dccdeb1d5060cd27de21a231db752
tolmany <jollydev0821@gmail.com>
2021-09-30 18:50:43 +0300

    Rosi Game: slow down timer and make it more precise

--- src/components/RosiGameAnimation/Timer.js
+++ src/components/RosiGameAnimation/Timer.js
@@ -1,36 +1,39 @@
 import { useEffect, useState } from 'react';
 
 // Crash factor = (elapsed time) * TIME_TO_FACTOR_RATIO
-const TIME_TO_FACTOR_RATIO = 0.5; // 1s = 0.5x
+const TIME_TO_FACTOR_RATIO = 0.1; // 1s = 0.1x
 const START_FACTOR = 1;
 
-const Timer = ({ pause, startTimeMs }) => {
+const Timer = ({ startTimeMs }) => {
   const startTime = new Date(startTimeMs);
-  const [elapsed, setElapsed] = useState(startTime.getTime());
-  const elapsedSeconds = (elapsed * TIME_TO_FACTOR_RATIO) / 1000 + START_FACTOR;
-  const wholePart = Math.trunc(elapsedSeconds);
-  // round up decimal part to the previous multiple of 5. So it would count 40, 45, 50, 55 ...
-  const decimalPart = Math.floor(
-    (elapsedSeconds - Math.floor(elapsedSeconds)) * 100
-  );
+  const [elapsed, setElapsed] = useState(0);
+  const factor = (elapsed / 1000) * TIME_TO_FACTOR_RATIO + START_FACTOR;
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (pause) {
-        return;
-      }
+    const intervalTime = 10;
+    let lastTime = Date.now();
+    let lastDelay = intervalTime;
+    let timeoutId;
 
-      setElapsed(e => e + 10);
-    }, 10);
+    const tick = () => {
+      let now = Date.now();
+      let timeDiff = now - lastTime;
 
-    return () => clearInterval(interval);
-  }, [pause]);
+      lastTime = now;
+      lastDelay = intervalTime + lastDelay - timeDiff;
+      timeoutId = setTimeout(tick, lastDelay);
 
-  return (
-    <span>
-      {wholePart}.{decimalPart < 10 ? `0${decimalPart}` : decimalPart}
-    </span>
-  );
+      // console.log(((lastTime - startTime.getTime()) * TIME_TO_FACTOR_RATIO) / 1000 + 1);
+      // console.log((now - lastDelay) - startTime.getTime());
+      setElapsed(now - startTime.getTime());
+    };
+
+    timeoutId = setTimeout(tick, 0);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return <span>{factor.toFixed(2)}</span>;
 };
 
 export default Timer;
