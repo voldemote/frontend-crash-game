1cb75e40cbf0e3ec32c9c4eae7f60b2a1bf2d1e5
tolmany <jollydev0821@gmail.com>
2021-09-30 17:16:19 +0500

    Mobile scroll to top (#431)

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ import ResetPassword from './screens/ResetPassword';
 import LandingPage from 'screens/LandingPage';
 import initTagManager from './config/gtm';
 import AudioContent from "./components/AudioContent";
+import ScrollToTop from 'utils/ScrollToTop';
 
 const { store, persistor } = configStore();
 
@@ -39,10 +40,11 @@ const App = () => {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <ConnectedRouter history={history}>
+          <ScrollToTop />
           <Navbar skipRoutes={[]} />
           <AlertBox />
           <Popup />
-          <AudioContent/>
+          <AudioContent />
           <Switch>
             <Route exact path={Routes.logout} component={Logout} />
             <Route
