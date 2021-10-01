ae5d6401d9037543bacaec67c40e09cdcd63bf1c
tolmany <jollydev0821@gmail.com>
2021-10-01 19:23:10 +0500

    updating the content home page

--- src/screens/LandingPage/SectionFour/index.js
+++ src/screens/LandingPage/SectionFour/index.js
@@ -10,24 +10,25 @@ const SectionFour = ({ classes }) => {
       <div className={classes.sectionNumber}>04</div>
       <div className={classes.textContainer}>
         <h2 className={classes.sectionHeading}>
-          What is the WFAIR Token and what can you do with it?
+          But what is the WFAIR Token and what can you do with it?
         </h2>
         <p className={classes.textParagraph}>
-          Have you always wanted to bet on your favourite Esports players? or
-          predict who will win the next US election? We have everything from
-          Sports betting, Live games in Vegas as well as chicken races and who
-          will win Big Brother 2022, our range of events are second-to-none and
-          are limited only by the imaginations of our players.
+          Good question, Jimmy. Think of it as your own in-game currency, think
+          of it as coins in your wallet or even a casino chip. The $WFAIR token
+          is our very own, in-house crypto and the best part is, it’s easy
+          Crypto.
         </p>
         <br />
         <p className={classes.textParagraph}>
-          We are built on Blockchain Technology. That means transparent betting
-          for our players, no more greedy bookmakers, no more high fees and odds
-          that are determined by the players, not the house. Betting on Wallfair
-          is as simple as searching for your favourite event, or why not even
-          create one of your own, jumping right into the action and simply
-          clicking the “place bet” button. Then sit back and watch your $WFAIR
-          tokens grow. The more you play, the more you earn.
+          You buy the tokens from our top up store, or one of our exchange
+          partners and that's it, you’re ready to go. Not only that, but we have
+          hidden more ways to earn more tokens everywhere throughout our
+          platform, either through rewards or hidden items and collectables. The
+          price for one $WFAIR Token is 0,20€ and will change after our official
+          token listing event with our exchange partners and users will get new
+          tokens every week as well as from referring friends, family and
+          completing feedback tasks to help the Wallfair team improve the user
+          journey for our valued players.
         </p>
       </div>
     </section>
--- src/screens/LandingPage/SectionTwo/index.js
+++ src/screens/LandingPage/SectionTwo/index.js
@@ -19,13 +19,24 @@ const SectionTwo = ({ classes }) => {
         </p>
         <br />
         <p className={classes.textParagraph}>
-          We are built on Blockchain Technology. That means transparent betting
-          for our players, no more greedy bookmakers, no more high fees and odds
-          that are determined by the players, not the house. Betting on Wallfair
-          is as simple as searching for your favourite event, or why not even
-          create one of your own, jumping right into the action and simply
-          clicking the “place bet” button. Then sit back and watch your $WFAIR
-          tokens grow. The more you play, the more you earn.
+          To ensure a level of quality and trust in our events, during Wallfair
+          Alpha we will be creating events exclusively with our partners and
+          trusted community members. As we move forward into a new era of
+          betting, we will open the flood gates to event creation for everyone,
+          yes, that means you, our valuable players. This means anyone can
+          create events at any time and start earning on their own creative
+          speculations.
+        </p>
+        <br />
+        <p className={classes.textParagraph}>
+          We are built on Blockchain Technology. Polygon to be precise. That
+          means transparent betting for our players, no more greedy bookmakers,
+          no more high fees and odds that are determined by the players, not the
+          house. Betting on Wallfair is as simple as searching for your
+          favourite event, or why not even create one of your own, jumping right
+          into the action and simply clicking the “place bet” button. Then sit
+          back and watch your $WFAIR tokens grow. The more you play, the more
+          you earn.
         </p>
       </div>
     </section>
