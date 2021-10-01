42d1c3cebb120ea0dcec8cc54dc3d1f268125b98
tolmany <jollydev0821@gmail.com>
2021-10-01 21:15:10 +0700

    Display a profit number

--- src/components/RosiGameAnimation/Timer.js
+++ src/components/RosiGameAnimation/Timer.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUserBet } from '../../store/selectors/rosi-game';
 
 // Crash factor = (elapsed time) * TIME_TO_FACTOR_RATIO
 const TIME_TO_FACTOR_RATIO = 0.1; // 1s = 0.1x
 const START_FACTOR = 1;
 
-function calcCrashFactorFromElapsedTime(timeDiff) {
+export function calcCrashFactorFromElapsedTime(timeDiff) {
   var offsetTime = 0;
   var offsetFactor = 0;
   var speed = 0;
@@ -37,8 +39,9 @@ function calcCrashFactorFromElapsedTime(timeDiff) {
   return (((timeDiff - offsetTime) / speed) * 0.01 + offsetFactor).toFixed(2); //currentCrashFactor
 }
 
-const Timer = ({ startTimeMs }) => {
+const Timer = ({ startTimeMs, showIncome = false }) => {
   const startTime = new Date(startTimeMs);
+  const bet = useSelector(selectUserBet);
   const [factor, setFactor] = useState(0);
 
   useEffect(() => {
@@ -56,7 +59,22 @@ const Timer = ({ startTimeMs }) => {
     return () => clearTimeout(intervalId);
   }, []);
 
-  return <span>{factor}</span>;
+  function renderProfit() {
+    if (!bet || !bet.amount) return null;
+    const profit = (bet.amount * factor - bet.amount).toFixed(0);
+    if (profit > 0) {
+      return <span className={'positive'}>+ {profit} WFAIR</span>;
+    }
+    if (profit === 0) {
+      return <span className={'zero'}>{profit} WFAIR</span>;
+    }
+    if (profit < 0) {
+      return <span className={'negative'}>- {profit} WFAIR</span>;
+    }
+    return null;
+  }
+
+  return showIncome ? renderProfit() : <span>{factor}</span>;
 };
 
 export default Timer;
