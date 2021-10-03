483218a38ee5d06a62d6b5c076b45c4623c33eaa
tolmany <jollydev0821@gmail.com>
2021-10-03 20:57:13 +0200

    changes on footer, popup disclaimer and normies page

--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -14,53 +14,58 @@ const Disclaimer = ({ className = '' }) => {
 
   return (
     <div className={classNames(styles.lightbox, className)}>
-      <div className={styles.content}>
-        <p>Disclaimer</p>
-        <p>
-          Wallfair.io serves informational, entertainment and educational
-          purposes only. We neither plan to take custody of any user's or
-          participant's fiat or cryptocurrency assets nor to host any events
-          that have not been prepared by or agreed with the team of Wallfair.
-          The users need to be of age (18+) and sound body and mind (meaning
-          unaffected by addiction or substances causing loss of control or
-          consciousness) to use the website. The participation in the events and
-          trades on the website is voluntary and final and the availability of
-          the platform may be affected by the online connection enabled by your
-          local internet provider. All questions and doubts can be addressed to
-          the team at <a href="mailto:hello@wallfair.io">hello@wallfair.io</a>.
+      {!readMore && (
+        <p className={styles.readmore} onClick={handleClickReadMore}>
+          Disclaimer
         </p>
-        {!readMore && (
-          <p className={styles.readmore} onClick={handleClickReadMore}>
-            READ MORE
-          </p>
-        )}
-        {readMore && (
-          <>
-            <p>
+      )}
+      {readMore && (
+        <div className={styles.content}>
+          <ol>
+            <li>
               Wallfair Ltd. is a company organized in accordance with the laws
               of Bailwick of Guernsey (Alderney), registration number 2024,
               located at 2 Victoria Street, St. Anne, GY9 3UF, Alderney Channel
-              Islands.
-            </p>
-            <p>
-              Wallfair Ltd. has pending applications for gambling licenses with
-              the Alderney Gambling Control Commission and the Curacao Gaming
-              Authority. Curacao Gaming licensing and supervision is controlled
-              by the Government of Curacao under National Decree No. 14 from 18.
-              August 1998 while the applicable law on Alderney is the Gambling
-              Law of 1999. Under these licensing ordinances, the license
-              applicant/holder is authorized to advertise and provide services
-              in such countries and territories that have issued own gambling
-              regulations or have been blacklisted by the regulatory
-              authorities. Wallfair Ltd. is closely observing the applicability
-              of the licensing regulations and taking all measures with the
-              purpose to comply with the applicable legal requirements. All
-              further questions and doubts can be addressed to the team at{' '}
-              <a href="mailto:hello@wallfair.io">hello@wallfair.io</a>
-            </p>
-          </>
-        )}
-      </div>
+              Islands. Wallfair Ltd. has pending applications for gambling
+              licenses with the Alderney Gambling Control Commission and the
+              Curacao Gaming Authority. Curacao Gaming licensing and supervision
+              is controlled by the Government of Curacao under National Decree
+              No. 14 from 18. August 1998 while the applicable law on Alderney
+              is the Gambling Law of 1999.
+            </li>
+            <li>
+              Wallfair.io is a blockchain-based platform built on Polygon chain
+              and incorporating the usability function of an ERC-20 token
+              (“WFAIR”) minted in Alderney and distributed through decentralized
+              exchanges as well as the Trustpad launchpad (www.trustpad.io).
+            </li>
+            <li>
+              The platform serves informational, entertainment and educational
+              purposes only.
+            </li>
+            <li>
+              The users need to be of age (18+) and sound body and mind (meaning
+              unaffected by addiction or substances causing loss of control or
+              consciousness) to use the website.
+            </li>
+            <li>
+              All bets, events and trades on the website are final and the
+              availability of the platform may be affected by the online
+              connection enabled by your local internet provider.
+            </li>
+            <li>
+              The dispute resolution as well as other functions on the site are
+              developed by the Wallfair core team and established in accordance
+              with the internal guidelines found on the website.
+            </li>
+            <li>
+              All further questions and doubts can be addressed to the team at{' '}
+              <a href="mailto:hello@wallfair.io">hello@wallfair.io</a> as well
+              as on the social media channels, e.g. Telegram and Twitter.
+            </li>
+          </ol>
+        </div>
+      )}
     </div>
   );
 };
