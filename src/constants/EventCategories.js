1a06b0c87c48ea62e99762878db5d229ab46bc26
tolmany <jollydev0821@gmail.com>
2021-10-06 19:28:55 +0200

    removed Vegas category and renamed News to Politics

--- src/constants/EventCategories.js
+++ src/constants/EventCategories.js
@@ -1,8 +1,7 @@
 import SportsImageIcon from '../data/icons/events-sports-icon.svg';
 import CryptoImageIcon from '../data/icons/events-other-icon.svg';
 import ControllerImageIcon from '../data/icons/controller.svg';
-import NewsImageIcon from '../data/icons/news.svg';
-import VegasImageIcon from '../data/icons/vegas.svg';
+import PoliticsImageIcon from '../data/icons/news.svg';
 import JokerImageIcon from '../data/icons/joker.svg';
 import TradingImageIcon from '../data/icons/trading.svg';
 import AllImageIcon from '../data/icons/events-all-icon.svg';
@@ -30,20 +29,14 @@ export const EVENT_CATEGORIES = [
     type: 'icon',
   },
   {
-    value: 'Vegas',
-    image: VegasImageIcon,
-    isActive: false,
-    type: 'icon',
-  },
-  {
     value: 'Sports',
     image: SportsImageIcon,
     isActive: false,
     type: 'icon',
   },
   {
-    value: 'News',
-    image: NewsImageIcon,
+    value: 'Politics',
+    image: PoliticsImageIcon,
     isActive: false,
     type: 'icon',
   },
@@ -82,19 +75,13 @@ export const LIVE_EVENTS_CATEGORIES = [
     type: 'image',
   },
   {
-    value: 'Vegas',
-    image: ApexLegendsImage,
-    isActive: false,
-    type: 'image',
-  },
-  {
     value: 'Sports',
     image: CSImage,
     isActive: false,
     type: 'image',
   },
   {
-    value: 'News',
+    value: 'Politics',
     image: FortniteImage,
     isActive: false,
     type: 'image',
