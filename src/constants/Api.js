8ac7660b2461a30bef49f5d5cff3de197d1b7807
tolmany <jollydev0821@gmail.com>
2021-10-06 10:09:14 +0200

    debug

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -10,6 +10,7 @@ export const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 export const NEWS_API_SSL_MODE =
   process.env.REACT_APP_NEWS_API_SSL_MODE === 'true';
 
+  console.log('BACKEND URL JE JEBOTE', BACKEND_URL)
 // Services
 export const API_AUTHENTICATION_REQUEST_SMS_URL = 'api/user/login';
 export const API_AUTHENTICATION_SAVE_ADD_INFO_URL =
