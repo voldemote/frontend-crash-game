aec305044c9f208d251eaa388caa890f4069c07d
tolmany <jollydev0821@gmail.com>
2021-10-12 19:58:12 +0500

    removing auto scroll focus for chat.

--- src/screens/Bet/index.js
+++ src/screens/Bet/index.js
@@ -242,9 +242,6 @@ const Bet = ({
 
   const onBetActionSwitch = newIndex => {
     setBetAction(newIndex);
-    if (newIndex === 0) {
-      mobileChatRef.current?.scrollIntoView({ behavior: 'smooth' });
-    }
   };
 
   const isLoggedIn = () => {
