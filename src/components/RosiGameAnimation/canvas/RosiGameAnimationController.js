e117ca3fdd29f83798b644c49f1011a762cf83ef
tolmany <jollydev0821@gmail.com>
2021-10-13 13:38:45 +0300

    Rosi Game: Update intervals

--- src/components/RosiGameAnimation/canvas/RosiGameAnimationController.js
+++ src/components/RosiGameAnimation/canvas/RosiGameAnimationController.js
@@ -4,11 +4,7 @@ import RosiAnimationBackground from './Background';
 import { CoinAnimation } from './CoinAndTrajectory';
 import TWEEN from '@tweenjs/tween.js';
 import CoinExplosion from './CoinExplosion';
-import {
-  calcCrashFactorFromElapsedTime,
-  isMobileRosiGame,
-  calcPercent,
-} from './utils';
+import { calcCrashFactorFromElapsedTime, isMobileRosiGame } from './utils';
 import CashedOutAnimation from './CashedOutAnimation';
 import PreparingRound from './PreparingRound';
 
@@ -17,13 +13,23 @@ PIXI.utils.skipHello();
 
 const intervals = [
   // fromFactor, toFactor, speed, elonFrame
-  [1, 1.2, 2, 0],
-  [1.15, 1.5, 3, 0],
-  [1.5, 2, 5, 1],
+  [1, 1.2, 1, 0],
+  [1.2, 1.5, 2, 0],
+  [1.5, 2, 4, 1],
   [2, 3, 7, 2],
-  [3, 3.5, 9, 3],
-  [3.5, 4, 11, 4],
-  [4, 4.5, 13, 5],
+  [3, 4, 9, 3],
+  [4, 5, 11, 4],
+  [5, 7.5, 13, 5],
+  [7.5, 10, 15, 5],
+  [10, 15, 19, 5],
+  [15, 20, 24, 5],
+  [20, 25, 28, 5],
+  [25, 30, 32, 5],
+  [30, 35, 36, 5],
+  [35, 40, 40, 5],
+  [40, 45, 44, 5],
+  [45, 50, 48, 5],
+  [50, 100, 60, 5],
 ];
 
 function loadAssets(loader) {
