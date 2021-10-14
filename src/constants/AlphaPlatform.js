e0183e44a25f472a92d1b93bdb8660ff9960d401
tolmany <jollydev0821@gmail.com>
2021-10-14 13:44:43 -0700

    Added alpha-platform howdoesit work

---
+++ src/constants/AlphaPlatform.js
@@ -0,0 +1,43 @@
+import First from '../data/backgrounds/alpha-platforms/alpha-platform1-bg.png';
+import Second from '../data/backgrounds/alpha-platforms/alpha-platform2-bg.png';
+import Third from '../data/backgrounds/alpha-platforms/alpha-platform3-bg.png';
+import Fourth from '../data/backgrounds/alpha-platforms/alpha-platform4-bg.png';
+import Routes from './Routes';
+
+export const ALPHA_PLATFORMS = [
+  {
+    background: First,
+    title: '01',
+    subtitle: '',
+    description: 'Sign up and you get 5000 WFAIR tokens to play',
+    active: false,
+    linkTo: '',
+  },
+  {
+    background: Second,
+    title: '02',
+    subtitle: '',
+    description:
+      'As you play you also earn rewards and climb on the leaderboard',
+    active: false,
+    linkTo: '',
+  },
+  {
+    background: Third,
+    title: '03',
+    subtitle: '',
+    description:
+      'Every week, the Top 10 on the leaderboard are rewarded real WFAIR tokens',
+    active: false,
+    linkTo: '',
+  },
+  {
+    background: Fourth,
+    title: '04',
+    subtitle: '',
+    description:
+      'You can sell the tokens or use them when our platform launches',
+    active: false,
+    linkTo: '',
+  },
+];
