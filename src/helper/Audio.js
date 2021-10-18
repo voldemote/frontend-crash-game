5d1f055e248bc1f9d5f176dc215a1bf795d59f43
tolmany <jollydev0821@gmail.com>
2021-10-18 20:02:00 +0700

    Add new audio

--- src/helper/Audio.js
+++ src/helper/Audio.js
@@ -1,6 +1,44 @@
+import { random } from 'lodash/number';
+
 export function playWinSound(volumeLevel) {
   try {
     const el = document.getElementById('audio-win');
+    if (el.play) {
+      el.volume = volumeLevel;
+      el.play();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function playBetSound(volumeLevel) {
+  try {
+    const el = document.getElementById('audio-bet');
+    if (el.play) {
+      el.volume = volumeLevel;
+      el.play();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function playLoseSound(volumeLevel) {
+  try {
+    const el = document.getElementById('audio-lose');
+    if (el.play) {
+      el.volume = volumeLevel;
+      el.play();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function playGameoverSound(volumeLevel) {
+  try {
+    const el = document.getElementById('audio-gameover');
     if (el.play) {
       el.volume = volumeLevel;
       el.play();
@@ -36,7 +74,11 @@ export function playFailSound(volumeLevel = 0.5) {
 
 export function playFlyingSound(volumeLevel = 0.5, seek = 0) {
   try {
-    const el = document.getElementById('audio-flying');
+    let el = document.getElementById('audio-flying');
+
+    if (random(0, 1, false) > 0) {
+      el = document.getElementById('audio-bgm');
+    }
     if (el.play) {
       el.volume = volumeLevel;
       el.play();
@@ -53,9 +95,14 @@ export function playFlyingSound(volumeLevel = 0.5, seek = 0) {
 export function stopFlyingSound() {
   try {
     const el = document.getElementById('audio-flying');
+    const el2 = document.getElementById('audio-bgm');
     if (el.play) {
       el.pause();
       el.currentTime = 0;
+    }
+    if (el2.play) {
+      el2.pause();
+      el2.currentTime = 0;
     }
   } catch (e) {
     console.error(e);
@@ -65,8 +112,10 @@ export function stopFlyingSound() {
 export function silenceAllSounds() {
   try {
     const el = document.getElementById('audio-flying');
+    const el2 = document.getElementById('audio-bgm');
     if (el.play) {
       el.volume = 0.0;
+      el2.volume = 0.0;
     }
   } catch (e) {
     console.error(e);
@@ -76,8 +125,10 @@ export function silenceAllSounds() {
 export function resetAllSounds() {
   try {
     const el = document.getElementById('audio-flying');
+    const el2 = document.getElementById('audio-bgm');
     if (el.play) {
       el.volume = 0.5;
+      el2.volume = 0.5;
     }
   } catch (e) {
     console.error(e);
