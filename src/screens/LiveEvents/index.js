12257e668a2a3c7b1edc3d3c5af48937b53a9c9c
tolmany <jollydev0821@gmail.com>
2021-10-21 10:46:14 -0700

    Added SHOW_UPCOMING_FEATURES env variable

--- src/screens/LiveEvents/index.js
+++ src/screens/LiveEvents/index.js
@@ -7,29 +7,32 @@ import LiveEventsScreenshotMobile from 'data/images/live-events-screenshot-mobil
 import styles from './styles.module.scss';
 
 const LiveEvents = () => {
+  const showUpcoming = process.env.SHOW_UPCOMING_FEATURES || false;
   const [categories, setCategories] = useState(EVENT_CATEGORIES);
 
   return (
     <BaseContainerWithNavbar withPaddingTop={true}>
-      <div className={styles.placeholderLiveEventsScreenshot}>
-        <div className={styles.inactivePlaceholder}>Coming Soon</div>
-        <img
-          src={LiveEventsScreenshot}
-          alt="live events screenshot"
-          className={styles.desktop}
+      {!showUpcoming ? (
+        <div className={styles.placeholderLiveEventsScreenshot}>
+          <div className={styles.inactivePlaceholder}>Coming Soon</div>
+          <img
+            src={LiveEventsScreenshot}
+            alt="live events screenshot"
+            className={styles.desktop}
+          />
+          <img
+            src={LiveEventsScreenshotMobile}
+            alt="live events screenshot mobile"
+            className={styles.mobile}
+          />
+        </div>
+      ) : (
+        <EventsContent
+          eventType="streamed"
+          categories={categories}
+          setCategories={setCategories}
         />
-        <img
-          src={LiveEventsScreenshotMobile}
-          alt="live events screenshot mobile"
-          className={styles.mobile}
-        />
-      </div>
-
-      {/* <EventsContent
-        eventType="streamed"
-        categories={categories}
-        setCategories={setCategories}
-      /> */}
+      )}
     </BaseContainerWithNavbar>
   );
 };
