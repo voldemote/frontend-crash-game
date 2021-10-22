82ad9567e979735cd945af1892c98992d11b7a4f
tolmany <jollydev0821@gmail.com>
2021-10-22 11:14:46 +0200

    extract events content into separate component for nonstreamed events

---
+++ src/components/Events/NonStreamedEventsContent/StatusTabs/index.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import React, { useEffect, useCallback, useState } from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import styles from './styles.module.scss';
+import { PopupActions } from 'store/actions/popup';
+import classNames from 'classnames';
+
+import { EventActions } from '../../../../store/actions/event';
+import SuggestEventButton from 'components/Events/SuggestEventButton';
+
+const TABS = {
+  current: 'current',
+  future: 'future',
+  past: 'past',
+};
+
+const StatusTabs = ({ onSelect = () => {}, preselected = TABS.current }) => {
+  const [activeTab, setActiveTab] = useState(preselected);
+
+  const setSelection = selection => {
+    setActiveTab(selection);
+    onSelect(selection);
+  };
+
+  const tabLabels = {
+    [TABS.current]: 'Current Events',
+    [TABS.future]: 'Future Events',
+    [TABS.past]: 'Past Events',
+  };
+
+  return (
+    <div className={styles.statusPicker}>
+      {Object.keys(TABS).map(tabName => (
+        <button
+          type="button"
+          className={classNames(
+            styles.tabButton,
+            activeTab === tabName && styles.tabButtonActive
+          )}
+          onClick={() => setSelection(tabName)}
+        >
+          {tabLabels[tabName]}
+        </button>
+      ))}
+      <SuggestEventButton externalClassNames={[styles.suggestEventButton]} />
+    </div>
+  );
+};
+
+export default StatusTabs;
