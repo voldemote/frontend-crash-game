5bc1c3eb491d39cc5c3c15dc3aacf22b6cc688a1
tolmany <jollydev0821@gmail.com>
2021-10-25 19:30:37 +0700

    Change color Adjust for mobile view

--- src/components/VolumeSlider/index.js
+++ src/components/VolumeSlider/index.js
@@ -1,15 +1,17 @@
 import styles from './styles.module.scss';
 
 import React from 'react';
-import { Slider } from '@material-ui/core';
+import { Slider, useMediaQuery } from '@material-ui/core';
 
 function VolumeSlider({ level = 0.5, handleChange = () => {} }) {
+  const isMobile = useMediaQuery('(max-width: 768px)');
   return (
     <Slider
       className={styles.slider}
       aria-label="Volume"
       value={level * 100}
       onChange={handleChange}
+      orientation={isMobile ? 'vertical' : 'horizontal'}
       min={0}
       max={100}
       sx={{
