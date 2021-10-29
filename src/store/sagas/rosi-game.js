0239b0ff10711a6f289bbe5cfbde4975e72d93dc
tolmany <jollydev0821@gmail.com>
2021-10-29 14:28:51 +0700

    Clean old audio

--- src/store/sagas/rosi-game.js
+++ src/store/sagas/rosi-game.js
@@ -1,4 +1,4 @@
-import { call, delay, put, select } from 'redux-saga/effects';
+import { delay, put } from 'redux-saga/effects';
 import { RosiGameActions } from '../actions/rosi-game';
 
 export const endGame = function* () {
@@ -7,8 +7,4 @@ export const endGame = function* () {
   yield put(RosiGameActions.endEndgamePeriod());
   yield put(RosiGameActions.resetInGameBets());
   yield put(RosiGameActions.resetCashedOut());
-};
-
-export const stopSound = function* () {
-  yield put(RosiGameActions.stopFlyingSound());
 };
