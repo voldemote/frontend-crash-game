1f83a7214b76cd4ea374954e83077079f887e177
tolmany <jollydev0821@gmail.com>
2021-10-29 15:35:36 +0700

    Bring back auto-cashout UI

--- src/components/PlaceBet/index.js
+++ src/components/PlaceBet/index.js
@@ -446,8 +446,59 @@ const PlaceBet = ({ connected, onBet, onCashout }) => {
               </div>
             </div>
           )}
+          <div className={styles.inputContainer}>
+            <label
+              className={classNames(
+                styles.label,
+                showCashoutWarning ? styles.warning : null
+              )}
+            >
+              Auto Cashout at
+            </label>
+            <div
+              className={classNames(
+                styles.cashedOutInputContainer,
+                showCashoutWarning ? styles.warning : null
+              )}
+            >
+              <Input
+                className={styles.input}
+                type={'number'}
+                value={!crashFactorDirty ? '1.00' : crashFactor}
+                onChange={onCrashFactorChange}
+                step={0.01}
+                min="1"
+                disabled={!user.isLoggedIn}
+              />
+              <span className={styles.eventTokenLabel}>
+                <span>x</span>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
+      {showCashoutWarning ? (
+        <div className={styles.error}>
+          <span>Betting less than 1 is not recommended. </span>
+          <span
+            data-for="rt"
+            className={styles.why}
+            data-tip="The multiplying factor defines your final reward.<br/>
+             A multiplier of 2x means twice the reward, when the game ends.<br/>
+              If the game ends before your multiplier,<br/> your amount invested is lost.<br/>"
+          >
+            Understand why.
+          </span>
+        </div>
+      ) : null}
+      <ReactTooltip
+        id={'rt'}
+        place="top"
+        effect="solid"
+        offset={{ bottom: 10 }}
+        multiline
+        className={styles.tooltip}
+      />
       {renderProfit()}
       {renderButton()}
       {renderMessage()}
