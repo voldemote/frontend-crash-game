0d770008e0185fcdee76bfe8ccba55a4063c8416
tolmany <jollydev0821@gmail.com>
2021-11-04 12:01:27 -0700

    Added elon wallpapers

---
+++ src/constants/Wallpapers.js
@@ -0,0 +1,66 @@
+import First from '../data/backgrounds/games/wallpaper1-bg.png';
+import Second from '../data/backgrounds/games/wallpaper2-bg.png';
+import Third from '../data/backgrounds/games/wallpaper3-bg.png';
+import Fourth from '../data/backgrounds/games/wallpaper4-bg.png';
+import Fifth from '../data/backgrounds/games/wallpaper5-bg.png';
+import Sixth from '../data/backgrounds/games/wallpaper6-bg.png';
+import Seventh from '../data/backgrounds/games/wallpaper7-bg.png';
+
+export const ELON_WALLPAPERS = [
+  {
+    background: First,
+    title: '',
+    subtitle: '',
+    description: '',
+    active: true,
+    linkTo: '',
+  },
+  {
+    background: Second,
+    title: '',
+    subtitle: '',
+    description: '',
+    active: true,
+    linkTo: '',
+  },
+  {
+    background: Third,
+    title: '',
+    subtitle: '',
+    description: '',
+    active: true,
+    linkTo: '',
+  },
+  {
+    background: Fourth,
+    title: '',
+    subtitle: '',
+    description: '',
+    active: true,
+    linkTo: '',
+  },
+  {
+    background: Fifth,
+    title: '',
+    subtitle: '',
+    description: '',
+    active: true,
+    linkTo: '',
+  },
+  {
+    background: Sixth,
+    title: '',
+    subtitle: '',
+    description: '',
+    active: true,
+    linkTo: '',
+  },
+  {
+    background: Seventh,
+    title: '',
+    subtitle: '',
+    description: '',
+    active: true,
+    linkTo: '',
+  },
+];
