948df73b66e78ee0cafe99e8b886f94d6dbdc814
tolmany <jollydev0821@gmail.com>
2021-11-05 15:29:30 +0100

    MVP2-679 - add boxes for global stats trades/volume

--- src/screens/Activities/index.js
+++ src/screens/Activities/index.js
@@ -3,14 +3,55 @@ import styles from './styles.module.scss';
 import ActivitiesTracker from '../../components/ActivitiesTracker';
 import ContentFooter from 'components/ContentFooter';
 
-import './swiper.scss'; //workaround for swiper module
+import './swiper.scss';
+import React from 'react'; //workaround for swiper module
 
 const Activities = () => {
   return (
     <BaseContainerWithNavbar withPaddingTop={true}>
       <div className={styles.container}>
-        <div className={styles.activitiesCategory}>
-          <span>Activities</span>
+        <div className={styles.globalStats}>
+          <div className={styles.statsBlock}>
+            <div className={styles.statItem}>
+              <div className={styles.statItemHead}>
+                <span>5</span> Game bets
+              </div>
+              <div className={styles.statItemHint}>last 24 hours</div>
+            </div>
+            <div className={styles.statItem}>
+              <div className={styles.statItemHead}>
+                <span>5</span> Game bets
+              </div>
+              <div className={styles.statItemHint}>last week</div>
+            </div>
+
+            <div className={styles.statItem}>
+              <div className={styles.statItemHead}>
+                <span>5</span> Game bets
+              </div>
+              <div className={styles.statItemHint}>all time</div>
+            </div>
+
+            <div className={styles.statItem}>
+              <div className={styles.statItemHead}>
+                <span>5</span> Game trades volume
+              </div>
+              <div className={styles.statItemHint}>last 24 hours</div>
+            </div>
+
+            <div className={styles.statItem}>
+              <div className={styles.statItemHead}>
+                <span>5</span> Game trades volume
+              </div>
+              <div className={styles.statItemHint}>last week</div>
+            </div>
+            <div className={styles.statItem}>
+              <div className={styles.statItemHead}>
+                <span>5</span> Game trades volume
+              </div>
+              <div className={styles.statItemHint}>all time</div>
+            </div>
+          </div>
         </div>
 
         <ActivitiesTracker
