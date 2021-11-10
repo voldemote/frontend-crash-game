c1812a64b5b141378fc84c2b55156d44fab46b22
tolmany <jollydev0821@gmail.com>
2021-11-10 10:24:32 +0100

    merge

--- src/components/RouletteGameAnimation/AnimationController.js
+++ src/components/RouletteGameAnimation/AnimationController.js
@@ -2,19 +2,15 @@ import { options } from 'components/EmailNotifications/options';
 import { init } from 'store/sagas/websockets';
 
 const innerWidth = 140;
-let sections = [
-  '£25',
-  '£15',
-  '£50',
-  '£1000',
-  '£25',
-  '£500',
-  '£15',
-  '£100',
-  '£1000',
-  '£15',
-  '£100',
-  '£500',
+let sections = [0.68, 0.3, 1, 2, 0.68, 0.3, 1, 2, 0.68, 0.3, 1, 1.95];
+let riskImages = [
+  '/images/roulette-game/1.svg',
+  '/images/roulette-game/2.svg',
+  '/images/roulette-game/3.svg',
+  '/images/roulette-game/4.svg',
+  '/images/roulette-game/5.svg',
+  '/images/roulette-game/6.svg',
+  '/images/roulette-game/7.svg',
 ];
 
 let colors = ['#0bf', '#fb0', '#bf0', '#b0f'];
@@ -52,14 +48,14 @@ class AnimationController {
         ctx.save();
         if (i == selected) {
           ctx.fillStyle = '#FFF';
-          ctx.shadowColor = '#FFF';
+          ctx.shadowColor = '#000';
           ctx.shadowBlur = this.r / 20;
         } else {
-          ctx.fillStyle = '#AAA';
+          ctx.fillStyle = '#000FF';
           ctx.shadowColor = '#000';
           ctx.shadowBlur = this.r / 100;
         }
-        ctx.font = (this.r / sections.length) * 1.6 + 'px serif';
+        ctx.font = (this.r / sections.length) * 1.6 + 'px PlusJakarta-Regular';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
         ctx.translate(cx, cy);
@@ -77,7 +73,7 @@ class AnimationController {
     ctx.shadowOffsetX = this.r / 80;
     ctx.shadowOffsetY = this.r / 80;
     ctx.shadowBlur = this.r / 40;
-    ctx.shadowColor = 'rgba(0,0,0,0.5)';
+    //ctx.shadowColor = 'rgba(0,0,0,0.5)';
     ctx.beginPath();
     ctx.arc(cx, cy, this.r * 1.025, 0, 2 * Math.PI, true);
     ctx.arc(cx, cy, this.r * 0.975, 0, 2 * Math.PI, false);
@@ -110,15 +106,8 @@ class AnimationController {
     ctx.fillStyle = '#F44';
     ctx.fill();
     ctx.clip();
-    var img = new Image();
-    img.src = '/images/roulette-game/1.svg';
-    img.onload = function () {
-      ctx.drawImage(img, cx, cy, 1000, 1000);
-    };
-
-    ctx.fill();
   }
-
+  //when calling repaint pass to the method the new index image from riskImages
   repaint(angle) {
     this.angle = angle;
     let cx = this.canvas.width / 2,
@@ -142,6 +131,12 @@ class AnimationController {
       cx - this.frame.width / 2,
       cy - this.frame.height / 2
     );
+    var img = new Image();
+    img.src = '/images/roulette-game/7.svg';
+    //check if image is loaded, if yes drawit
+    //img.onload = function () {
+    ctx.drawImage(img, 369, 120, 150, 150);
+    //};
   }
 
   spinTo(winner = (Math.random() * sections.length) | 0, duration = 5000) {
