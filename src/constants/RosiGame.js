d3331d6f37eb9c673ba6104875c8d69f3b65c74e
tolmany <jollydev0821@gmail.com>
2021-11-11 11:12:48 +0000

    game ids all from one place

--- src/constants/RosiGame.js
+++ src/constants/RosiGame.js
@@ -1,5 +1,7 @@
-export const ROSI_GAME_EVENT_ID = '614381d74f78686665a5bb76';
-export const PLINKO_GAME_EVENT_ID = '618a821bd90fd22298859bc5';
+import { GAMES } from './Games';
+
+export const ROSI_GAME_EVENT_ID = GAMES.elonGame.id;
+export const PLINKO_GAME_EVENT_ID = GAMES.plinko.id;
 export const ROSI_GAME_AFTER_CRASH_DELAY = 2000;
 export const ROSI_GAME_MOBILE_BREAKPOINT = 480; // in pixels
 
