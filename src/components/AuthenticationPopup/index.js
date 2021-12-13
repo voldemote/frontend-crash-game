bba6e11cee35f4fde58dc12849a5de8e37612f9a
tolmany <jollydev0821@gmail.com>
2021-12-13 14:47:39 +0100

    - update recaptcha

--- src/components/AuthenticationPopup/index.js
+++ src/components/AuthenticationPopup/index.js
@@ -8,17 +8,12 @@ import { RECAPTCHA_KEY } from 'constants/Api';
 const AuthenticationPopup = ({ authenticationType, preloadEmailSignUp }) => {
 
   return (
-    <div
-      className={classNames(styles.registration)}
-    >
-      <h2 className={styles.title}>{authenticationType === 'register' ? 'Final Step' : 'Login'}</h2>
+    <div className={classNames(styles.registration)}>
+      <h2 className={styles.title}>
+        {authenticationType === 'register' ? 'Final Step' : 'Login'}
+      </h2>
       <div className={styles.form}>
-        <GoogleReCaptchaProvider
-          reCaptchaKey={RECAPTCHA_KEY}
-          scriptProps={{
-            async: true,
-          }}
-        >
+        <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_KEY}>
           <Authentication
             authenticationType={authenticationType}
             preloadEmailSignUp={preloadEmailSignUp}
