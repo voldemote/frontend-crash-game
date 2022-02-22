13e067a4d1ee07866702d296be0eff855eb4050b
tolmany <jollydev0821@gmail.com>
2022-02-22 08:31:38 -0800

    Aded first banner

--- src/components/CustomCarousel/CustomCarousel.js
+++ src/components/CustomCarousel/CustomCarousel.js
@@ -11,6 +11,23 @@ import authState from 'constants/AuthState';
 import Routes from 'constants/Routes';
 import classNames from 'classnames';
 
+import BackgroundFirst from '../../data/images/carousel/bg-first.png';
+import BackgroundCard from '../../data/images/carousel/bg-card.png';
+import BannerImage1 from '../../data/images/carousel/banner-img1.png';
+import {ReactComponent as LockIcon} from '../../data/images/carousel/lock.svg';
+import Button from 'components/Button';
+
+const BottomBanner = (title, price) => {
+  return (
+    <div className={styles.bottomBanner}>
+      <img src={BannerImage1} alt='' />
+      <div className={styles.bottomBannerContent}>
+        <p className={styles.title}>T.P just earned</p>
+        <p className={styles.price}>239 WFAIR</p>
+      </div>
+    </div>
+  )
+}
 const CustomCarousel = ({loggedIn, showWalletDepositPopup, handleKycInfoVisible, setOpenDrawer, userId, showPopup, phoneConfirmed}) => {
   const dispatch = useDispatch();
   const history = useHistory();
@@ -58,7 +75,7 @@ const CustomCarousel = ({loggedIn, showWalletDepositPopup, handleKycInfoVisible,
     }
   };
 
-  const renderDesktopSlides = () => {
+  const renderSlides = () => {
     return (
       <Carousel
         className={styles.carousel}
@@ -71,13 +88,52 @@ const CustomCarousel = ({loggedIn, showWalletDepositPopup, handleKycInfoVisible,
         showStatus={false}
         showIndicators={false}
         showThumbs={false}
-        onClickItem={onClickItem}
+        onClickItem={() => {}}
       >
-      <div>
-        <img
-          alt=""
-          src={`https://files.wallfair.io/landingpage-carousel/slide_create_own_event.jpg?v=3`}
-        />
+      <div className={styles.container}>
+        <div className={styles.topContainer}>
+          <img
+            className={styles.backgroundImg}
+            alt=""
+            src={BackgroundFirst}
+          />
+          <div className={styles.firstContainer}>
+            <div>
+              <h2>CREATE YOUR OWN EVENT AND <span className={styles.secondTitle}>MAKE MONEY!</span></h2>
+              <p className={styles.description}>
+                Create events within 2 minutes, share them with 
+                your friends and earn real money.
+              </p>
+              <div className={styles.buttonWrapper}>
+                <Button className={styles.button}>Create an event</Button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.secondContainer}>
+            <div className={styles.card}>
+              <img className={styles.cardImage} src={BackgroundCard} alt='' />
+              <div className={styles.liveReact}>
+                <span className={styles.oval} />
+                <p>LIVE</p>
+              </div>
+              <div className={styles.cardContent}>
+                <h2>My Birthday Burger Championship IV</h2>
+                <div className={styles.lockWrapper}>
+                  <LockIcon />
+                  <p>INVITE ONLY</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div className={styles.bottomBannerContainner}>
+          <BottomBanner/>
+          <BottomBanner/>
+          <BottomBanner/>
+          <BottomBanner/>
+          <BottomBanner/>
+        </div>
       </div>
       {/* <div>
         <img
@@ -133,11 +189,8 @@ const CustomCarousel = ({loggedIn, showWalletDepositPopup, handleKycInfoVisible,
   }
 
   return <>
-    <div className={classNames(styles.carouselContainer, styles.desktop)}>
-      {renderDesktopSlides()}
-    </div>
-    <div className={classNames(styles.carouselContainer, styles.mobile)}>
-      {renderMobileSlides()}
+    <div className={classNames(styles.carouselContainer)}>
+      {renderSlides()}
     </div>
   </>;
 };
