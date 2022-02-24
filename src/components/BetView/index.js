0ad784e421f1e1ce9add57ada6faae7df41660b9
tolmany <jollydev0821@gmail.com>
2022-02-24 17:20:20 +0100

    adjusted event screen on mobile, set default bet to 10 PFAIR

--- src/components/BetView/index.js
+++ src/components/BetView/index.js
@@ -55,7 +55,7 @@ const BetView = ({
   // Slider is also using 2800 as max value
   const BALANCE_NOT_LOGGED = 2800;
   const { currency, balance } = useSelector(selectUser);
-  const defaultBetValue = 50;
+  const defaultBetValue = 10;
   const bet = event.bet;
   const state = _.get(bet, 'status');
   const auth = useSelector(state => state.authentication);
@@ -628,7 +628,7 @@ const BetView = ({
               // styles.nonStreamedQuestion
             )}
           >
-            <span>{bet.market_question}</span>
+            <span className={styles.eventTitle}>{bet.market_question}</span>
             <div className={styles.betDescription}>{bet.description}</div>
           </div>
           {renderStateConditionalContent()}
