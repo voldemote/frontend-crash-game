cfabb96035f85908fbc76a67d546f42ad8722489
tolmany <jollydev0821@gmail.com>
2022-02-24 14:20:41 +0100

    Update the event status in bet card for past events

--- src/components/BetCard/index.js
+++ src/components/BetCard/index.js
@@ -3,9 +3,8 @@ import styles from './styles.module.scss';
 import classNames from 'classnames';
 import { calculateTimeLeft } from '../../helper/Time';
 import { EVENT_CATEGORIES } from '../../constants/EventCategories';
-import LiveBadge from 'components/LiveBadge';
-import HotBetBadge from 'components/HotBetBadge';
-import HotBetBadgeTheme from 'components/HotBetBadge/HotBetBadgeTheme';
+import BetState from 'constants/BetState';
+
 const BetCard = ({
   betId,
   onClick,
@@ -18,6 +17,7 @@ const BetCard = ({
   outcomes,
   category,
   item,
+  state,
   isBookmarked = false,
   tags,
   small = false,
@@ -28,13 +28,13 @@ const BetCard = ({
     };
   };
 
-  const getStickerStyle = category => {
-    const cat = EVENT_CATEGORIES.find(c => c.value === category);
-    if (!cat) return {};
-    return {
-      backgroundImage: 'url("' + cat.image + '")',
-    };
-  };
+  const eventState = {
+    [BetState.disputed]: 'Event disputed',
+    [BetState.closed]: 'Event closed',
+    [BetState.canceled]: 'Event cancelled',
+    [BetState.resolved]: 'Event resolved',
+    [BetState.waitingResolution]: 'Event waiting for resoultion',
+  }
 
   const [timeLeft, setTimeLeft] = useState(
     calculateTimeLeft(new Date(eventEnd))
@@ -49,11 +49,13 @@ const BetCard = ({
   }, [eventEnd]);
 
   return (
-    <div className={classNames(styles.betCardContainer, eventCardClass)} onClick={onClick}>
+    <div
+      className={classNames(styles.betCardContainer, eventCardClass)}
+      onClick={onClick}
+    >
       <div className={styles.picture} style={getEventCardStyle()} />
       <div className={classNames(styles.picture, styles.overlay)} />
       <div className={classNames(styles.betCard)}>
-      
         <div className={styles.badgeContainer}>
           {/* <LiveBadge className={styles.liveBadge} /> */}
           {/* <HotBetBadge className={styles.hotBadge} theme={HotBetBadgeTheme.opacity05}>3,212 Trade Vol.</HotBetBadge> */}
@@ -61,36 +63,37 @@ const BetCard = ({
         <div className={styles.titleContainer}>
           <span className={styles.title}>{title}</span>
           <div className={styles.tags}>
-            {
-              (!small && tags) && tags.map(tag => <span>#{tag.name}</span>)
-            }
+            {!small && tags && tags.map(tag => <span>#{tag.name}</span>)}
           </div>
         </div>
       </div>
-      
+
       {eventEnd && new Date(eventEnd) > new Date() ? (
         <div className={styles.timerContainer}>
-          <div className={classNames(styles.contentWrapper, small ? styles.smallTimer : null)}>
-            {!small && <span className={styles.timerLabel}>Event ends in:{' '}</span>}
-            {small && <span className={styles.timerLabel}>Ends in:{' '}</span>}
-            <span className={styles.timerValue}>
-              {timeLeft?.days || 0}
-            </span>
-            <span className={styles.timerUnit}>days{' '}</span>
-            <span className={styles.timerValue}>
-              {timeLeft?.hours || 0}
-            </span>
-            <span className={styles.timerUnit}>hrs{' '}</span>
-            <span className={styles.timerValue}>
-              {timeLeft?.minutes || 0}
-            </span>
-            <span className={styles.timerUnit}>min{' '}</span>
+          <div
+            className={classNames(
+              styles.contentWrapper,
+              small ? styles.smallTimer : null
+            )}
+          >
+            {!small && (
+              <span className={styles.timerLabel}>Event ends in: </span>
+            )}
+            {small && <span className={styles.timerLabel}>Ends in: </span>}
+            <span className={styles.timerValue}>{timeLeft?.days || 0}</span>
+            <span className={styles.timerUnit}>days </span>
+            <span className={styles.timerValue}>{timeLeft?.hours || 0}</span>
+            <span className={styles.timerUnit}>hrs </span>
+            <span className={styles.timerValue}>{timeLeft?.minutes || 0}</span>
+            <span className={styles.timerUnit}>min </span>
           </div>
         </div>
       ) : (
         <div className={styles.timerContainer}>
           <div className={styles.contentWrapper}>
-            <span className={styles.timerLabel}>Event ended</span>
+            <span className={styles.timerLabel}>
+              {eventState[state] || 'Event ended'}
+            </span>
           </div>
         </div>
       )}
