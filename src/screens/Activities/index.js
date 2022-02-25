824a2fb9e583dea08e676dc345c6184883967611
tolmany <jollydev0821@gmail.com>
2022-02-25 00:05:29 -0800

    Update top categories on activities

--- src/screens/Activities/index.js
+++ src/screens/Activities/index.js
@@ -80,51 +80,52 @@ const Activities = ({showBets = false}) => {
             <div className={styles.statsBlock}>
               <div className={styles.statItem}>
                 <div className={styles.statItemHead}>
-                  <span>{toNumericString(data24h?.trades)}</span> 24h # of bets
+                  <span className={styles.title}>24h # of bets</span>
+                  <span>{toNumericString(data24h?.trades)}</span> 
                 </div>
                 {/*<div className={styles.statItemHint}>last 24 hours</div>*/}
               </div>
               <div className={styles.statItem}>
                 <div className={styles.statItemHead}>
-                  <span>{toNumericString(dataLastWeek?.trades)}</span> 7d # of
-                  bets
+                  <span className={styles.title}>7d # of bets</span>
+                  <span>{toNumericString(dataLastWeek?.trades)}</span> 
                 </div>
                 {/*<div className={styles.statItemHint}>last week</div>*/}
               </div>
 
               <div className={styles.statItem}>
                 <div className={styles.statItemHead}>
-                  <span>{toNumericString(dataAllTime?.trades)}</span> Total # of
-                  bets
+                  <span className={styles.title}>Total # of bets</span>
+                  <span>{toNumericString(dataAllTime?.trades)}</span> 
                 </div>
                 {/*<div className={styles.statItemHint}>all time</div>*/}
               </div>
 
               <div className={styles.statItem}>
                 <div className={styles.statItemHead}>
+                  <span className={styles.title}>24h volume</span>
                   <span>
                     {toNumericString(data24h?.volume)} {currencyDisplay(TOKEN_NAME)}
-                  </span>{' '}
-                  24h volume
+                  </span>
                 </div>
                 {/*<div className={styles.statItemHint}>last 24 hours</div>*/}
               </div>
 
               <div className={styles.statItem}>
                 <div className={styles.statItemHead}>
+                  <span className={styles.title}>7d volume</span>
                   <span>
                     {toNumericString(dataLastWeek?.volume)} {currencyDisplay(TOKEN_NAME)}
-                  </span>{' '}
-                  7d volume
+                  </span>
                 </div>
                 {/*<div className={styles.statItemHint}>last week</div>*/}
               </div>
               <div className={styles.statItem}>
                 <div className={styles.statItemHead}>
+                  <span className={styles.title}>Total volume (IN {currencyDisplay(TOKEN_NAME)})</span>
                   <span>
-                    {toNumericString(dataAllTime?.volume)} {currencyDisplay(TOKEN_NAME)}
-                  </span>{' '}
-                  Total volume
+                    {toNumericString(dataAllTime?.volume)}
+                  </span>
                 </div>
                 {/*<div className={styles.statItemHint}>all time</div>*/}
               </div>
