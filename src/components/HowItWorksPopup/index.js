855d683b1bf469ea274fddb1742736d333146c38
tolmany <jollydev0821@gmail.com>
2022-03-07 13:29:08 +0100

    Restyle how it works popup for mobile

--- src/components/HowItWorksPopup/index.js
+++ src/components/HowItWorksPopup/index.js
@@ -8,9 +8,12 @@ import Highlight from 'components/Highlight';
 import { useDispatch } from 'react-redux';
 import { PopupActions } from 'store/actions/popup';
 import PopupTheme from 'components/Popup/PopupTheme';
+import Coins from '../../data/images/carousel/coins-2.png';
+import { useMediaQuery } from '@material-ui/core';
 
 const HowItWorksPopup = () => {
   const dispatch = useDispatch();
+  const isMobile = useMediaQuery('(max-width: 992px)');
 
   const handleClick = () => {
     dispatch(PopupActions.show({ popupType: PopupTheme.auth }))
@@ -19,33 +22,46 @@ const HowItWorksPopup = () => {
   return (
     <div className={styles.howItWorks}>
       <h1 className={styles.title}>
-        Sign up & get 100 PFAIR for free
-        <Highlight
-          className={styles.highlight}
-          highlightType={HighlightType.highlightTitle}
-        />
+        Sign up & get{' '}
+        <span className={styles.highlighted}>
+          100 PFAIR for free{' '}
+          <Highlight
+            className={styles.highlight}
+            highlightType={HighlightType.highlightTitle}
+          />
+        </span>
       </h1>
       <div className={styles.description}>
         Create your account and you get <b>100 PFAIR for free</b> and have the
         chance to win <br /> <b>150 USD every day</b>! Create your own events
-        and earn money!<br /> And this is how it works:
+        and earn money!
+        <br /> And this is how it works:
       </div>
       <div className={styles.steps}>
         <div className={styles.step}>
           <div className={styles.number}>1</div>
           Create your Account
         </div>
-        <Icon iconType={IconType.arrowRight} className={styles.chevron} />
+        <Icon
+          iconType={isMobile ? IconType.arrowDown : IconType.arrowRight}
+          className={styles.chevron}
+        />
         <div className={styles.step}>
           <div className={styles.number}>2</div>
           Verify your Email
         </div>
-        <Icon iconType={IconType.arrowRight} className={styles.chevron} />
+        <Icon
+          iconType={isMobile ? IconType.arrowDown : IconType.arrowRight}
+          className={styles.chevron}
+        />
         <div className={styles.step}>
           <div className={styles.number}>3</div>
           Create Event & Share
         </div>
-        <Icon iconType={IconType.arrowRight} className={styles.chevron} />
+        <Icon
+          iconType={isMobile ? IconType.arrowDown : IconType.arrowRight}
+          className={styles.chevron}
+        />
         <div className={styles.step}>
           <div className={styles.number}>4</div>
           Earn Money
@@ -60,6 +76,8 @@ const HowItWorksPopup = () => {
       >
         <span>Sign up and create Event</span>
       </Button>
+
+      <img src={Coins} className={styles.coins} alt="coins" />
     </div>
   );
 };
